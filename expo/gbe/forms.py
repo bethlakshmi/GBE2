64384af3aa7a4c9a6f7d95f1e11c57391629b716
Jon P. Kiparsky <jpk@jons-mbp.westell.com>
2014-04-05 03:54:03 -0400

    Minor logic fix in actbid validation

--- expo/gbe/forms.py
+++ expo/gbe/forms.py
@@ -99,8 +99,7 @@ class ActBidForm(forms.ModelForm):
       super(ActBidForm, self).__init__(*args, **kwargs)
       # this is dynamic for the festival experience, it's a supporting table
       # with a list of related festivals that may grow or change in time
-      n = 8
-      for festival in festival_list:
+      for (n, festival) in enumerate(festival_list, 8):
         try:
           initial_experience = PerformerFestivals.objects.get(
           			actbid=kwargs['initial']['bidid'],festival=festival[0]).experience
@@ -108,7 +107,7 @@ class ActBidForm(forms.ModelForm):
           initial_experience = "No"
         self.fields.insert(n,festival[0],forms.ChoiceField(choices=festival_experience, 
         			initial=initial_experience, label=festival[1]))
-        n += 1
+
 
     required_css_class = 'required'
     error_css_class = 'error'
@@ -188,29 +187,28 @@ class ActBidForm(forms.ModelForm):
       else:
         super(ActBidForm, self).clean()
         is_group = self.cleaned_data.get('is_group')
-        name = self.cleaned_data.get('video_link')
+        name = self.cleaned_data.get('name')
         others = self.cleaned_data.get('other_performers')
-        if is_group == "Yes":
-          group_err = False
+        group_err = False
+        if is_group == "Yes" and others == '':
+            group_err = True
+            self._errors['other_performers']=self.error_class(actbid_otherperformers_missing)
+            self._errors['is_group']=self.error_class(actbid_group_wrong)
         if name == '':
-          group_err = True
-          self._errors['name']=self.error_class(actbid_name_missing)
-        if others == '':
-          group_err = True
-          self._errors['other_performers']=self.error_class(actbid_otherperformers_missing)
+            group_err = True
+            self._errors['name']=self.error_class(actbid_name_missing)
         if group_err:
-          self._errors['is_group']=self.error_class(actbid_group_wrong)
-          raise forms.ValidationError(actbid_group_error)
+            raise forms.ValidationError(actbid_group_error)
         video_choice = self.cleaned_data.get('video_choice')
         video_link = self.cleaned_data.get('video_link')
         if video_choice != "0" and video_link == '':
-          self._errors['video_choice']=self.error_class(video_error1)
-          self._errors['video_link']=self.error_class(video_error2)
-          raise forms.ValidationError(video_error3)
+            self._errors['video_choice']=self.error_class(video_error1)
+            self._errors['video_link']=self.error_class(video_error2)
+            raise forms.ValidationError(video_error3)
         for festival in festival_list:
-          match = False
-          for answer in festival_experience:
-            match = self.cleaned_data.get(festival[0]) == answer[0]
+            match = False
+            for answer in festival_experience:
+                match = self.cleaned_data.get(festival[0]) == answer[0]
 
       return self.cleaned_data
 
