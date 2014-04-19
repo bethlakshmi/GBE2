518e24b56a3c8bb9bc5e1cd4e62c1b6a86af13e8
Jon Kiparsky <jpk@jons-mbp.westell.com>
2014-04-19 02:07:03 -0400

    Made urls tolerant of presence or absence of final '/'.

--- expo/gbe/urls.py
+++ expo/gbe/urls.py
@@ -2,34 +2,47 @@ from django.conf.urls import patterns, url
 from django.contrib.auth.views import login, logout
 from gbe import views
 
+
+# NOTE: in general, url patterns should end with '/?$'. This 
+# means "match the preceding patter, plus an optional final '?', 
+# and no other characters following. So '^foo/?$' matches on
+# "foo" or "foo/", but not on "foo/bar" or "foo!". 
+# Which is what we usually want. 
+
 urlpatterns = patterns ('', 
-                        url(r'^$', 
+                        url(r'^/?$', 
                             views.index),
-                        url(r'index.html', 
+                        url(r'index.html/?', 
                             views.index),
-                        url(r'techinfo', 
+                        url(r'^techinfo/?$', 
                             views.techinfo),
-                        url(r'^performer/create$',
+                        url(r'^profile/?$',
+                            views.index),
+                        url(r'^profile/(\d)/?$',
+                            views.view_profile),
+                        url(r'^act/create/?$',
+                            views.bid_act),
+                        url(r'^performer/create/?$',
                             views.register_as_performer),
 
-                        url(r'^event/(P<event_id>\d+)/$', 
+                        url(r'^event/(P<event_id>\d+)/?$', 
                             views.event), 
-                        url(r'^act/(?P<act_id>\d+)/$', 
+                        url(r'^act/(?P<act_id>\d+)/?$', 
                             views.act),
-                        url(r'^bid/(?P<type>\w+)/', 
+                        url(r'^bid/(?P<type>\w+)/?$', 
                             views.bid),
-                        url(r'^editbid/(?P<type>\w+)/(?P<bid_id>\d+)/', 
+                        url(r'^editbid/(?P<type>\w+)/(?P<bid_id>\d+)/?$', 
                             views.bid),
                         url(r'^bid/(?P<type>\w+)-(?P<response>\w+)', 
                             views.bid_response),
-                        url(r'^bid/index', 
+                        url(r'^bid/index/?$', 
                             views.test),
                         url(r'^login/,*$', login),
-                        url(r'^accounts/profile/$', 
+                        url(r'^accounts/profile/?$', 
                             views.update_profile),
-                        url(r'^accounts/register/$',
+                        url(r'^accounts/register/?$',
                             views.register),
-                        url(r'^accounts/login$', login), 
-                        url(r'^accounts/logout/$', logout),
-                        url(r'update_profile/$', views.update_profile),
+                        url(r'^accounts/login/?$', login), 
+                        url(r'^accounts/logout/?$', logout),
+                        url(r'update_profile/?$', views.update_profile),
                         ) 
