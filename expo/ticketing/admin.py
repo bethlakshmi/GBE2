bec261b06d50276e48e0a01f2f0bd65f494b48cb
Marcus DeBoyz <marcus.deboyz@gmail.com>
2014-06-06 16:48:58 -0700

    Setup Admin Site for Ticketing

--- expo/ticketing/admin.py
+++ expo/ticketing/admin.py
@@ -1,8 +1,15 @@
 # 
 # models.py - Contains Django code for the built-in Admin webpage
-# edited by mdb 4/25/2014
+# edited by mdb 6/6/2014
 #
 
 from django.contrib import admin
+from ticketing.models import *
 
 # Register your models here.
+
+admin.site.register( BrownPaperSettings )
+admin.site.register( BrownPaperEvents )
+admin.site.register( TicketItem )
+admin.site.register( Purchaser )
+
