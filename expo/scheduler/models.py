b1addfd288068094e6579099e7d821cb7dea02a7
Jon Kiparsky <jon.kiparsky@gmail.com>
2014-10-12 14:33:59 -0400

    Fix for #144. Note: must install django-model-utils Easiest way is to use pip: $ pip install django-model-utils (Will have to ensure that this can work on webfactional)Will have to ensure that this can work on webfactional)

--- expo/scheduler/models.py
+++ expo/scheduler/models.py
@@ -2,8 +2,7 @@ from django.db import models
 from gbe.expomodelfields import DurationField
 from django.core.validators import RegexValidator
 from datetime import timedelta
-
-# all literal text including option sets lives in gbetext.py
+from model_utils.managers import InheritanceManager
 from gbetext import *
 
 
@@ -18,6 +17,7 @@ class Schedulable(models.Model):
     commitments for a particular person, or for a particular event, or for a block
     of time - so this is a pretty flexible idea)
     '''
+    objects = InheritanceManager()
     @property 
     def duration(self):
         return self._duration
@@ -38,6 +38,7 @@ class ResourceItem (models.Model):
     '''
     The payload for a resource
     '''
+    objects = InheritanceManager()
     @property 
     def _name(self):
         return self.sched_name
@@ -47,7 +48,10 @@ class Resource(models.Model):
     A person, place, or thing that can be allocated for an event. 
     A resource has a payload and properties derived from that payload. 
     '''
-    item = models.ForeignKey(ResourceItem)
+    objects = InheritanceManager()
+    @property
+    def item(self):
+        return self._item
     @property
     def name(self):
         return self.item.sched_name
@@ -56,14 +60,40 @@ class LocationItem(ResourceItem):
     '''
     A wrapper class for a conference location
     '''
+    objects = InheritanceManager()
     pass
 
 class Location(Resource):
     '''
     A resource which is a location. 
     '''
+    objects = InheritanceManager()
+    _item = models.ForeignKey(LocationItem)
+
+class WorkerItem(ResourceItem):
+    '''
+    Wrapper class for a person as resource (staff/volunteer/teacher)
+    '''
+    objects = InheritanceManager()
     pass
-    
+
+class Worker (Resource):
+    '''
+    objects = InheritanceManager()
+    An allocatable person
+    '''
+    _item = models.ForeignKey(WorkerItem)
+
+class EquipmentItem(ResourceItem):
+    objects = InheritanceManager()
+    pass
+
+class Equipment(Resource):
+    '''
+    An allocatable thing
+    '''
+    objects = InheritanceManager()
+    _item = models.ForeignKey(EquipmentItem)
 
 class EventItem (models.Model):
     '''
@@ -71,6 +101,7 @@ class EventItem (models.Model):
     The EventItem MUST NOT impose any DB usage on its implementing model classes. 
     ALL requirements must be stated as properties.
     '''
+    objects = InheritanceManager()
     @property 
     def duration(self):
         return self.sched_duration
@@ -79,6 +110,7 @@ class Event (Schedulable):
     '''
     An Event is a schedulable item with a conference model item as its payload. 
     '''
+    objects = InheritanceManager()
     @property
     def duration(self):
         return self.item._duration
@@ -92,7 +124,7 @@ class ResourceAllocation(Schedulable):
     Joins a particular Resource to a particular Event
     ResourceAllocations get their scheduling data from their Event. 
     '''
-
+    objects = InheritanceManager()
     event = models.ForeignKey(Event, related_name="resources_allocated")
     resource = models.ForeignKey(Resource, related_name="allocations")
 
