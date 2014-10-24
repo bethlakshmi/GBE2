04b8a05c11e2ead82f4f17f73f3f62a52a0d014c
bethlakshmi <bethlakshmi@gmail.com>
2014-10-23 21:34:20 -0400

    Just some nifty admin things for Scratch
    Makes it easy to sort and look at admin without Betty having to do much.

--- expo/gbe/admin.py
+++ expo/gbe/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 from gbe.models import *
 
-
+class BidAdmin(admin.ModelAdmin):
+    list_display = (str, 'submitted', 'accepted')
+    list_filter = ['submitted', 'accepted']
+    
 admin.site.register( Profile )
 admin.site.register( Biddable )
-admin.site.register( Act )
+admin.site.register( Act, BidAdmin )
+admin.site.register( Class, BidAdmin )
+admin.site.register( Vendor, BidAdmin )
+admin.site.register( Volunteer, BidAdmin )
 admin.site.register( Show )
-admin.site.register( Class )
 admin.site.register( Room )
 admin.site.register( ClassProposal )
 admin.site.register( BidEvaluation )
@@ -20,8 +25,6 @@ admin.site.register( ProfilePreferences )
 admin.site.register( Persona )
 admin.site.register( Combo )
 admin.site.register( Troupe )
-admin.site.register( Vendor )
-admin.site.register( Volunteer )
 admin.site.register( ConferenceVolunteer )
 admin.site.register( GenericEvent )
 admin.site.register( Event )
