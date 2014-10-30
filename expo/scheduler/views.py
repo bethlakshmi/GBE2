c87518710d666c6748e750a12f9735b047d81e77
Jon Kiparsky <jon.kiparsky@gmail.com>
2014-10-30 17:41:49 -0400

    Can change time/location of an event

--- expo/scheduler/views.py
+++ expo/scheduler/views.py
@@ -176,31 +176,35 @@ def edit_event(request, eventitem_id):
                # Creating a new scheduler.Event and allocating a room
             event_form = EventScheduleForm(request.POST, 
                                      prefix='event')
-            if (event_form.is_valid()  and True):
-                s_event=event_form.save(commit=False)
-                s_event.eventitem = item
-                data = event_form.cleaned_data
-                s_event.save()            
-                from gbe.models import Room
-                li  = Room.objects.get(name=data.get('location'))
-                
-                try:
-                    res = Location.objects.get(_item=li)
-                except:
-                    res = Location()
-                    res._item = li.locationitem
-                    res.save()        
+        else:
+            event_form = EventScheduleForm(request.POST, 
+                                           instance = item.scheduler_events.all()[0],
+                                           prefix='event')
+        if (event_form.is_valid()  and True):
+            s_event=event_form.save(commit=False)
+            s_event.eventitem = item
+            data = event_form.cleaned_data
+            s_event.save()            
+            from gbe.models import Room
+            li  = Room.objects.get(name=data.get('location'))
+            
+            try:
+                res = Location.objects.get(_item=li)
+            except:
+                res = Location()
+                res._item = li.locationitem
+                res.save()        
         
 
-                loc_allocation = ResourceAllocation()
-                loc_allocation.event = s_event
-                loc_allocation.resource = res
-                loc_allocation.save()
+            loc_allocation = ResourceAllocation()
+            loc_allocation.event = s_event
+            loc_allocation.resource = res
+            loc_allocation.save()
                 # next: set duration on child
             
-                return HttpResponseRedirect(reverse('home', urlconf='gbe.urls'))
-            else:
-                return HttpResponseRedirect('/')
+            return HttpResponseRedirect(reverse('home', urlconf='gbe.urls'))
+        else:
+            return HttpResponseRedirect('/')
     eventitem_view = get_event_display_info(eventitem_id)
     template = 'scheduler/event_schedule.tmpl'
     eventform = EventScheduleForm( prefix='event')
