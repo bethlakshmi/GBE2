e1c6a52cb903d469632fb0f44a3261248987838a
bethlakshmi <bethlakshmi@gmail.com>
2014-11-22 23:17:10 -0500

    remove down message

--- expo/expo/settings.py
+++ expo/expo/settings.py
@@ -62,8 +62,8 @@ FILE_UPLOAD_HANDLERS = ("django.core.files.uploadhandler.MemoryFileUploadHandler
 
 
 #uncomment this to take the site down
-ROOT_URLCONF = 'expo.downconf'
-# ROOT_URLCONF = 'expo.urls'
+#ROOT_URLCONF = 'expo.downconf'
+ROOT_URLCONF = 'expo.urls'
 
 WSGI_APPLICATION = 'expo.wsgi.application'
 
