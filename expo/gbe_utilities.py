394d024cfac3b1358c399cb648ba449b7c5ec07d
Jon P. Kiparsky <jpk@Jons-MacBook-Pro.local>
2015-01-31 14:54:39 -0500

    Added a function to create rehearsal slots from the shell. Should work correctly, feel free to use it when setting up a local db.

--- expo/gbe_utilities.py
+++ expo/gbe_utilities.py
@@ -1,6 +1,26 @@
 # functions to use from the admin in maintaining the gbe project
 
 
+def create_standard_rehearsal_slots(shows):
+    '''
+    caller has to find the shows (as scheduler.Events, usually first 4 listed in .objects.all)
+    '''
+    import gbe.models as conf
+    import scheduler.models as sched
+    from datetime import timedelta
+    hour = timedelta(seconds = 3600)
+    for show in shows:
+        for i in range (1,4):
+            ge = conf.GenericEvent(title = '%s: Tech Rehearsal' %str(show), type = 'Rehearsal Slot', duration=3600) 
+            ge.save()
+            child = sched.Event(eventitem=ge, starttime = show.starttime- ( i  * hour))
+            child.save()
+            child.set_location(show.location)
+            child.save()
+            ec = sched.EventContainer(parent_event = show, child_event=child)
+            ec.save()
+
+
 def rebuild_privileges():
     '''
     Reads the privilege set from gbetext and creates the correct groups to make these work
