251dd1d0310b93afa5e63c86130673a9fc7e8671
bethlakshmi <bethlakshmi@gmail.com>
2015-03-29 21:52:22 -0400

    Fixed previous checkin
    that was the wrong checkin.  This is what’s in downconf on the site
    now.  Tested and working better than a few hours ago.

--- expo/expo/downconf.py
+++ expo/expo/downconf.py
@@ -1,15 +1,15 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
+from django.contrib.auth.views import *
+from gbe import views
 from django.conf import settings
-from django.conf.urls.static import static
 
-from django.contrib import admin
-admin.autodiscover()
+# NOTE: "urlconf" for taking site down for maintenance
 
-urlpatterns = patterns('',
-                       url(r'^', include('gbe.urls', namespace = 'gbe')), 
-                       url(r'^admin/', include(admin.site.urls)),
-                       url(r'^', include('ticketing.urls', namespace = 'ticketing')),
-                       url(r'^', include('scheduler.urls', namespace = 'scheduler')),
-                       url(r'^', include('gbe.report_urls', namespace = 'reporting')),
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+urlpatterns = patterns ('', 
+                        (r'^uploads/(?P<path>.*)$', 'django.views.static.serve', {
+                                 'document_root': settings.MEDIA_ROOT}),
+# landing page
+                        url(r'^.*$', 
+                            views.down, name = 'down'),
+                       )
