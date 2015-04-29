0dc5d89563dd7a1946423c24ac02fbd5905cf6b1
bethlakshmi <bethlakshmi@gmail.com>
2015-04-29 00:59:43 -0400

    Login @ Special Menu
    reconstructed logic for the special menu and go login links working.

--- expo/gbe/menu.py
+++ expo/gbe/menu.py
@@ -5,6 +5,8 @@ from cms.menu_bases import CMSAttachMenu
 from menus.base import Menu, NavigationNode
 from menus.menu_pool import menu_pool
 
+from gbe.functions import *
+from gbe.models import *
 
 class BidsMenu(CMSAttachMenu):
     name = _("I Want To")  # give the menu a name this is required.
@@ -30,27 +32,53 @@ class BidsMenu(CMSAttachMenu):
         return nodes
     
 class LoginMenu(Menu):
-    name = _("I Want To")  # give the menu a name this is required.
+    name = _("Your Account")  # give the menu a name this is required.
 
     def get_nodes(self, request):
         """
-        This method is used to build the menu tree.
+        menus for all users or potential users to do account management
         """
         nodes = []
  
-        node1 = NavigationNode(
-            "Logout",
-            reverse('gbe:logout'),
-            1
-            )
-        node2 = NavigationNode(
-            "Update Profile",
-            reverse('gbe:profile_update'),
-            2
-            )
-        nodes.append(node1)
-        nodes.append(node2)
+        nodes.append(NavigationNode(_("Your Account"), "", 1,
+                                    attr={'visible_for_anonymous': False} ))
+        nodes.append(NavigationNode(_("Update Profile"), reverse('gbe:profile_update'), 2, 1,
+                                    attr={'visible_for_anonymous': False}))
+        nodes.append(NavigationNode(_("Logout"), reverse('gbe:logout'), 3, 1,
+                                    attr={'visible_for_anonymous': False}))
+        nodes.append(NavigationNode(_("Join Today"), "", 4,
+                                    attr={'visible_for_authenticated': False} ))
+        nodes.append(NavigationNode(_("Register"), reverse('gbe:register'), 5, 4,
+                                    attr={'visible_for_authenticated': False}))
+        nodes.append(NavigationNode(_("Login"), reverse('gbe:login'), 6, 4,
+                                    attr={'visible_for_authenticated': False} ))
+
         return nodes
 
+class SpecialMenu(Menu):
+    name = _("Special")
+    
+    def get_nodes(self, request):
+        """
+        populate for users based on profile.  Users must have special privileges to use this
+        """
+        nodes = []
+        user = request.user
+        
+        if user.is_authenticated() and validate_profile(request) and user.profile.special_privs:
+            nodes.append(NavigationNode(_("Special"), "", 1 ))
+            nodes.append(NavigationNode(_("Reporting"), reverse('reporting:report_list'), 2, 1 ))
+            for priv in user.profile.special_privs:
+                n=3
+                
+                if priv["url"] and len(priv["url"]) > 0:
+                    nodes.append(NavigationNode(priv["title"], priv["url"], n, 1))
+                    n = n+1
+                    
+        return nodes
+        
+        
+        
 menu_pool.register_menu(BidsMenu) # register the menu.
+menu_pool.register_menu(SpecialMenu) # register the menu.
 menu_pool.register_menu(LoginMenu) # register the menu.
