e797e237388d574f1b2fa6f36b6aee16fe397a53
bethlakshmi <bethlakshmi@gmail.com>
2015-06-28 18:37:28 -0400

    Go Fund Me Plugin
    Now that I have mastered easy plugins, I’m going nuts, this is easier
    than snippets.

--- expo/gbe/cms_plugins.py
+++ expo/gbe/cms_plugins.py
@@ -21,6 +21,13 @@ class SubscribeEmailPlugin(CMSPluginBase):
     name = _("Subscribe to Email")  # name of the plugin in the interface
     render_template = loader.get_template('gbe/email_subscribe.tmpl')
 
+class GoFundMePlugin(CMSPluginBase):
+    model = CMSPlugin
+    module = _("GBE Plugins")
+    name = _("Go Fund Me Widget")  # name of the plugin in the interface
+    render_template = loader.get_template('gbe/go_fund_me.tmpl')
+
 
 plugin_pool.register_plugin(ContactFormPlugin)  # register the plugin
 plugin_pool.register_plugin(SubscribeEmailPlugin)  # register the plugin
+plugin_pool.register_plugin(GoFundMePlugin)  # register the plugin
