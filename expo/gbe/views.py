b98b1b262098399226bde256d031fad71e6c319e
Jon Kiparsky <jon.kiparsky@gmail.com>
2015-07-25 21:36:54 -0400

    More fixing

--- expo/gbe/views.py
+++ expo/gbe/views.py
@@ -2366,7 +2366,6 @@ def create_event(request, event_type):
                        'view_header_text':event_create_text[event_type] })
 
 def handle_user_contact_email(request):
-    import pdb; pdb.set_trace()
     return_redirect = HttpResponseRedirect(reverse('home',
                                                    urlconf='gbe.urls', 
                                                    args = []))
@@ -2375,9 +2374,10 @@ def handle_user_contact_email(request):
     form = ContactForm(request.POST)
     if not form.is_valid():
         return return_redirect
-    name = form.get('name', 'UNKNOWN USER')
-    from_address = form.get('email', 'UNKNOWN ADDRESS')
-    message = form.get('message', 'UNKNOWN MESSAGE')
+    data = form.cleaned_data
+    name = data.get('name', 'UNKNOWN USER')
+    from_address = data.get('email', 'UNKNOWN ADDRESS')
+    message = data.get('message', 'UNKNOWN MESSAGE')
     send_user_contact_email(name, from_address, message)
     return return_redirect
 
