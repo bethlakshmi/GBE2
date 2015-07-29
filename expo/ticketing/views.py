731eeec46e2070603bd77a23a726349bdeed62ff
bethlakshmi <bethlakshmi@gmail.com>
2015-07-29 18:51:28 -0400

    pep8 of views

--- expo/ticketing/views.py
+++ expo/ticketing/views.py
@@ -1,4 +1,4 @@
-# 
+#
 # views.py - Contains Django Views for Ticketing
 # edited by mdb 8/18/2014
 # updated by BB 7/26/2015
@@ -14,22 +14,22 @@ from ticketing.brown_paper import *
 from gbe.functions import *
 import pytz
 
-# Create your views here.
 
 def index(request):
     '''
-    Represents the view for the main screen.  This will eventually be the 
+    Represents the view for the main screen.  This will eventually be the
     equivalent of cost.php from the old site.
     '''
-    
-    ticket_items =  TicketItem.objects.all()
-    
+
+    ticket_items = TicketItem.objects.all()
+
     context = {'ticket_items': ticket_items,
-               'user_id':request.user.id,
+               'user_id': request.user.id,
                'site_name': get_current_site(request).name
                }
     return render(request, 'ticketing/purchase_tickets.tmpl', context)
-    
+
+
 def ticket_items(request):
     '''
     Represents the view for working with ticket items.  This will have a
@@ -37,13 +37,14 @@ def ticket_items(request):
     '''
     if not validate_perms(request, ('Ticketing - Admin', )):
         raise Http404
-            
+
     if 'Import' in request.POST:
         import_ticket_items()
-        
+
     events = BrownPaperEvents.objects.all()
-    context = {'events' : events}
+    context = {'events': events}
     return render(request, r'ticketing/ticket_items.tmpl', context)
+
 
 def transactions(request):
     '''
@@ -52,88 +53,93 @@ def transactions(request):
     '''
     if not validate_perms(request, ('Ticketing - Transactions', )):
         raise Http404
-     
+
     count = -1
     error = ''
-    
+
     if ('Sync' in request.POST):
         try:
             count = process_bpt_order_list()
         except Exception as e:
             error = 'Error processing transactions:  ' + str(e)
-        
+
     transactions = Transaction.objects.all()
     purchasers = Purchaser.objects.all()
     sync_time = get_bpt_last_poll_time()
-    
-    context = {'transactions' : transactions, 'purchasers' : purchasers, 
-        'sync_time' : sync_time, 'error' : error, 'count' : count}
-    return render(request, r'ticketing/transactions.tmpl', context) 
-    
-    
+
+    context = {'transactions': transactions,
+               'purchasers': purchasers,
+               'sync_time': sync_time,
+               'error': error,
+               'count': count}
+    return render(request, r'ticketing/transactions.tmpl', context)
+
+
 def import_ticket_items():
     '''
-    Function is used to initiate an import from BPT or other sources of 
+    Function is used to initiate an import from BPT or other sources of
     new Ticket Items.  It will not override existing items.
     '''
     import_item_list = get_bpt_price_list()
     db_item_list = TicketItem.objects.all()
-    
+
     for i_item in import_item_list:
-        if all(db_item.ticket_id != i_item.ticket_id for db_item in db_item_list):
+        if all(db_item.ticket_id != i_item.ticket_id
+               for db_item in db_item_list):
             i_item.save()
-            
+
+
 def ticket_item_edit(request, item_id=None):
     '''
     Used to display a form for editing ticket, adding or removing ticket items.
     '''
     if not validate_perms(request, ('Ticketing - Admin', )):
         raise Http404
-        
+
     error = ''
 
     if (request.method == 'POST'):
-    
+
         if 'delete_item' in request.POST:
-            # Delete this item based on the item_id in the URL
-            
-            item = TicketItem.objects.filter(id=item_id)[0]
-            if (item == None):
-                return HttpResponseRedirect(reverse('ticket_items', urlconf='ticketing.urls'))
-            
-            # Check to see if ticket item is used in a transaction before deleting.
-            
-            trans_exists = False    
+
+            item = get_object_or_404(TicketItem, id=item_id)
+
+            # Check to see if ticket item is used in a
+            # transaction before deleting.
+
+            trans_exists = False
             for trans in Transaction.objects.all():
                 print "%s %s" % (trans.ticket_item.ticket_id, item.ticket_id)
                 if (trans.ticket_item.ticket_id == item.ticket_id):
                     trans_exists = True
                     break
-            
+
             if (not trans_exists):
                 item.delete()
-                return HttpResponseRedirect(reverse('ticket_items', urlconf='ticketing.urls'))
+                return HttpResponseRedirect(reverse('ticket_items',
+                                                    urlconf='ticketing.urls'))
             else:
-                error = 'ERROR:  Cannot remove Ticket Item:  It is used in a Transaction.'
+                error = 'ERROR:  Cannot remove Ticket Item:  \
+                        It is used in a Transaction.'
                 form = TicketItemForm(instance=item)
-                
+
         else:
             # save the item using the Forms API
-    
+
             form = TicketItemForm(request.POST)
             if form.is_valid():
                 form.save(str(request.user))
                 form.save_m2m()
-                return HttpResponseRedirect(reverse('ticket_items', urlconf='ticketing.urls'))
+                return HttpResponseRedirect(reverse('ticket_items',
+                                                    urlconf='ticketing.urls'))
     else:
-        if (item_id != None):
+        if (item_id is not None):
             item = get_object_or_404(TicketItem, id=item_id)
             form = TicketItemForm(instance=item)
         else:
             form = TicketItemForm()
 
-
-    context = {'forms': [form,], 'error': error, 'can_delete': True} 
+    context = {'forms': [form], 'error': error, 'can_delete': True}
     return render(request, r'ticketing/ticket_item_edit.tmpl', context)
 
 
@@ -144,21 +150,22 @@ def bptevent_edit(request, event_id):
     '''
     if not validate_perms(request, ('Ticketing - Admin', )):
         raise Http404
-        
+
     error = ''
 
     event = get_object_or_404(BrownPaperEvents, id=event_id)
-        
+
     if (request.method == 'POST'):
-    
+
         # save the item using the Forms API
         form = BPTEventForm(request.POST, instance=event)
 
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect(reverse('ticket_items', urlconf='ticketing.urls'))
+            return HttpResponseRedirect(reverse('ticket_items',
+                                                urlconf='ticketing.urls'))
     else:
         form = BPTEventForm(instance=event)
 
-    context = {'forms': [form,], 'error': error, 'can_delete': False} 
+    context = {'forms': [form], 'error': error, 'can_delete': False}
     return render(request, r'ticketing/ticket_item_edit.tmpl', context)
