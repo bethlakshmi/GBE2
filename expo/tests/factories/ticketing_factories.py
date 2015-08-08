b447def281ecb32c8bcdb13bc919221a5259c2ea
bethlakshmi <bethlakshmi@gmail.com>
2015-08-08 11:01:47 -0400

    First set of ticket item tests
    all passing.

--- expo/tests/factories/ticketing_factories.py
+++ expo/tests/factories/ticketing_factories.py
@@ -11,3 +11,13 @@ class BrownPaperEventsFactory(DjangoModelFactory):
         model = tickets.BrownPaperEvents
     bpt_event_id = "111111"
     conference = SubFactory(gbe_factories.ConferenceFactory)
+
+class TicketItemFactory(DjangoModelFactory):
+    class Meta:
+        model = tickets.TicketItem
+    bpt_event = SubFactory(BrownPaperEventsFactory)
+    ticket_id = "111111-222222"
+    title = "Test Ticket Item"
+    description = "Describing Test Ticket Item"
+    cost = 99.99
+    modified_by = "Ticket Item Mock"
--- expo/tests/ticketing/test_edit_ticket.py
+++ expo/tests/ticketing/test_edit_ticket.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import Group
 from unittest import TestCase
 from django.test.client import RequestFactory
 from django.test import Client
-from ticketing.views import bptevent_edit
+from ticketing.views import ticket_item_edit
 from tests.factories import gbe_factories, ticketing_factories
 from tests.functions.gbe_functions import location
 import mock
@@ -20,66 +20,64 @@ class TestEditBPTEvent(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
         self.client = Client()
-        self.bpt_event = ticketing_factories.BrownPaperEventsFactory.create()
+        self.ticketitem = ticketing_factories.TicketItemFactory.create()
         group, created = Group.objects.get_or_create(name='Ticketing - Admin')
         self.privileged_user = gbe_factories.ProfileFactory.create().user_object
         self.privileged_user.groups.add(group)
 
-    def get_bptevent_form(self):
+    def get_ticketitem_form(self):
         return {
-                'primary': True,
-                'act_submission_event': True,
-                'vendor_submission_event': True,
-                'linked_events': [],
-                'include_conference': True,
-                'include_most': True,
-                'badgeable': True,
-                'ticket_style': 'ticket style',
-                'conference': self.bpt_event.conference.pk
+                'ticket_id': "333333-444444",
+                'title': "Title from Form",
+                'description': "Description from Form",
+                'active': False,
+                'cost': 1.01,
+                'bpt_event': self.ticketitem.bpt_event.pk
         }
     
     @nt.raises(Http404)
-    def test_edit_event_user_is_not_ticketing(self):
+    def test_edit_ticket_user_is_not_ticketing(self):
         '''
             The user does not have the right privileges.  Fail with a 404
         '''
         user = gbe_factories.ProfileFactory.create().user_object
-        request = self.factory.get('/ticketing/bptevent_edit/%d'%self.bpt_event.pk)
+        request = self.factory.get('/ticketing/ticket_item_edit/%d'%self.ticketitem.pk)
         request.user = user
-        response = bptevent_edit(request, self.bpt_event.pk)
+        response = ticket_item_edit(request, self.ticketitem.pk)
 
     @nt.raises(Http404)        
-    def test_edit_event_bad_bptevent(self):
+    def test_edit_ticket_bad_ticketitem(self):
         '''
-           Unknown event submitted by valid user, should have an error
+           Unknown ticket item submitted by valid user, should have an error
            and resend same form (status 200)
         '''
-        request = self.factory.get('/ticketing/bptevent_edit/200')
+        request = self.factory.get('/ticketing/ticket_item_edit/200')
         request.user =  self.privileged_user
-        response = bptevent_edit(request, 200)
+        response = ticket_item_edit(request, 200)
 
-    def test_event_edit_post_form_all_good(self):
+    def test_ticket_edit_post_form_all_good(self):
         '''
             Good form, good user, return the main edit page
         '''
-        request = self.factory.post('/ticketing/bptevent_edit/%d'%self.bpt_event.pk,
-                                    self.get_bptevent_form())
+        request = self.factory.post('/ticketing/ticket_item_edit/%d'%self.ticketitem.pk,
+                                    self.get_ticketitem_form())
         request.user = self.privileged_user
-        response = bptevent_edit(request, self.bpt_event.pk)
+        response = ticket_item_edit(request, self.ticketitem.pk)
+        print(response)
         nt.assert_equal(response.status_code, 302)
         nt.assert_equal(location(response), '/ticketing/ticket_items')
 
 
-    def test_event_edit_post_form_bad_event(self):
+    def test_ticket_edit_post_form_bad_bptevent(self):
         '''
             Invalid form data submitted, fail with error and return form
         '''
-        error_form = self.get_bptevent_form()
-        error_form['linked_events'] = -1
-        request = self.factory.post('/ticketing/bptevent_edit/%d'%self.bpt_event.pk,
+        error_form = self.get_ticketitem_form()
+        error_form['bpt_event'] = -1
+        request = self.factory.post('/ticketing/ticket_item_edit/%d'%self.ticketitem.pk,
                                     error_form)
         request.user = self.privileged_user
-        response = bptevent_edit(request, self.bpt_event.pk)
+        response = ticket_item_edit(request, self.ticketitem.pk)
         nt.assert_equal(response.status_code, 200)
         nt.assert_true('Edit Ticketing' in response.content)
-        nt.assert_true(error_form.get('ticket_style') in response.content)
+        nt.assert_true(error_form.get('title') in response.content)
