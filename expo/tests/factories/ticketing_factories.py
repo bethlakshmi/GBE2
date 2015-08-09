9e4af086df57eb25c23d7e0f672b6446125d5818
bethlakshmi <bethlakshmi@gmail.com>
2015-08-09 09:43:04 -0400

    Complete tests for edit ticket item

--- expo/tests/factories/ticketing_factories.py
+++ expo/tests/factories/ticketing_factories.py
@@ -4,7 +4,7 @@ from factory import SubFactory
 import ticketing.models as tickets
 import gbe.models as conf
 from tests.factories import gbe_factories
-
+from django.utils import timezone
 
 class BrownPaperEventsFactory(DjangoModelFactory):
     class Meta:
@@ -21,3 +21,29 @@ class TicketItemFactory(DjangoModelFactory):
     description = "Describing Test Ticket Item"
     cost = 99.99
     modified_by = "Ticket Item Mock"
+    
+class PurchaserFactory(DjangoModelFactory):
+    class Meta:
+        model = tickets.Purchaser
+    first_name = "Betty"
+    last_name = "Blaize"
+    address = "12 Betty Lane"
+    city = "Boston"
+    state = "MA"
+    zip = "12312"
+    country = "USA"
+    email = "purchaseemail@test.com"
+    phone = "111-222-3333"
+    matched_to_user = SubFactory(gbe_factories.UserFactory)
+    
+class TransactionFactory(DjangoModelFactory):
+    class Meta:
+        model = tickets.Transaction
+    ticket_item = SubFactory(TicketItemFactory)
+    purchaser = SubFactory(PurchaserFactory)
+    amount = 99.99
+    order_date = timezone.now()
+    shipping_method = ""
+    order_notes = ""
+    reference = ""
+    payment_source = ""
--- expo/tests/ticketing/test_edit_ticket.py
+++ expo/tests/ticketing/test_edit_ticket.py
@@ -55,6 +55,25 @@ class TestEditBPTEvent(TestCase):
         request.user =  self.privileged_user
         response = ticket_item_edit(request, 200)
 
+    def test_get_new_ticketitem(self):
+        '''
+           good user gets new ticket form, all is good.
+        '''
+        request = self.factory.get('/ticketing/ticket_item_edit')
+        request.user =  self.privileged_user
+        response = ticket_item_edit(request)
+        nt.assert_equal(response.status_code, 200)
+
+    def test_edit_ticketitem(self):
+        '''
+           good user gets form to edit existing ticket, all is good.
+        '''
+        self.ticketitem.save()
+        request = self.factory.get('/ticketing/ticket_item_edit/%d'%self.ticketitem.pk)
+        request.user =  self.privileged_user
+        response = ticket_item_edit(request, self.ticketitem.pk)
+        nt.assert_equal(response.status_code, 200)
+
     def test_ticket_edit_post_form_all_good(self):
         '''
             Good form, good user, return the main edit page
@@ -63,7 +82,18 @@ class TestEditBPTEvent(TestCase):
                                     self.get_ticketitem_form())
         request.user = self.privileged_user
         response = ticket_item_edit(request, self.ticketitem.pk)
-        print(response)
+        nt.assert_equal(response.status_code, 302)
+        nt.assert_equal(location(response), '/ticketing/ticket_items')
+
+    def test_ticket_add_post_form_all_good(self):
+        '''
+            Good form, good user, return the main edit page
+        '''
+        new_ticket = self.get_ticketitem_form()
+        request = self.factory.post('/ticketing/ticket_item_edit/',
+                                    new_ticket)
+        request.user = self.privileged_user
+        response = ticket_item_edit(request)
         nt.assert_equal(response.status_code, 302)
         nt.assert_equal(location(response), '/ticketing/ticket_items')
 
@@ -81,3 +111,49 @@ class TestEditBPTEvent(TestCase):
         nt.assert_equal(response.status_code, 200)
         nt.assert_true('Edit Ticketing' in response.content)
         nt.assert_true(error_form.get('title') in response.content)
+
+    def test_ticket_form_delete(self):
+        '''
+            Good form, good user, delete item and return to main page
+        '''
+        delete_ticket = self.get_ticketitem_form()
+        delete_ticket['delete_item'] = ''
+        delete_me = ticketing_factories.TicketItemFactory.create()
+        delete_me.ticket_id = "444444-555555"
+        delete_me.save()
+        request = self.factory.post('/ticketing/ticket_item_edit/%d'%delete_me.pk,
+                                    delete_ticket)
+        request.user = self.privileged_user
+        response = ticket_item_edit(request, delete_me.pk)
+        nt.assert_equal(response.status_code, 302)
+        nt.assert_equal(location(response), '/ticketing/ticket_items')
+
+    @nt.raises(Http404)
+    def test_ticket_form_delete_missing_item(self):
+        '''
+            Good form, good user, delete item that doesn't exist - get an error
+        '''
+        delete_ticket = self.get_ticketitem_form()
+        delete_ticket['delete_item'] = ''
+        request = self.factory.post('/ticketing/ticket_item_edit/%d'%200,
+                                    delete_ticket)
+        request.user = self.privileged_user
+        response = ticket_item_edit(request, 200)
+
+    def test_delete_ticket_with_transactions(self):
+        '''
+            Attempt to delete a ticket item that has a transaction
+            Get a failure, return to edit page
+        '''
+        delete_ticket = self.get_ticketitem_form()
+        delete_ticket['delete_item'] = ''
+        transaction = ticketing_factories.TransactionFactory.create()
+        transaction.save()
+        self.ticketitem.save()
+        request = self.factory.post('/ticketing/ticket_item_edit/%d'%transaction.ticket_item.pk,
+                                    delete_ticket)
+        request.user = self.privileged_user
+        response = ticket_item_edit(request, transaction.ticket_item.pk)
+        nt.assert_equal(response.status_code, 200)
+        nt.assert_true('Edit Ticketing' in response.content)
+        nt.assert_true('ERROR' in response.content)
