9a86ba8afdd7ba85b97f6a912ef0075cc9141757
Jon Kiparsky <jon.kiparsky@gmail.com>
2015-08-29 14:38:39 -0400

    Fixed tab order on login page

--- expo/gbe/templatetags/gbe_tags.py
+++ expo/gbe/templatetags/gbe_tags.py
@@ -7,7 +7,7 @@ register = template.Library()
 def mailchimp():
     if settings.MC_API_KEY == 'TEST':
         return {'have_mc': False}
-    return {'mc_api_url': settings.MC_API_URL, 
+    return {'mc_api_url': settings.MC_API_URL,
             'mc_api_user': settings.MC_API_USER,
             'mc_api_id': settings.MC_API_ID,
             'have_mc': True,
