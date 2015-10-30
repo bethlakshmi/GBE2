b86fa4f55b61c887e6a7d7263cfeef831f575010
Jon Kiparsky <jon@kiparsky.net>
2015-10-30 01:04:20 -0400

    Adding tests

---
+++ expo/tests/gbe/test_conference.py
@@ -0,0 +1,26 @@
+from tests.factories.gbe_factories import (
+    ConferenceFactory
+)
+
+from gbe.models import Conference
+
+import nose.tools as nt
+
+def test_by_slug_gets_conf_for_given_slug():
+    conf = ConferenceFactory.create(conference_slug="foo",
+                                    status="not_ongoing")
+    other_conf = ConferenceFactory.create(conference_slug="bar",
+                                          status="ongoing")
+    nt.assert_equal(type(conf).by_slug("foo"), conf)
+
+
+def test_by_slug_gets_default_if_bad_slug_given():
+    Conference.objects.all().delete()
+    conf = ConferenceFactory.create(conference_slug="foo",
+                                    status="not_ongoing")
+    other_conf = ConferenceFactory.create(conference_slug="bar",
+                                          status="ongoing")
+    nt.assert_equal(type(conf).by_slug("quux"), other_conf)
+
+    
+    
