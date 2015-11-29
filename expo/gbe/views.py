02ad4e27e15dab943a3b836694e51a78a07c34ea
bethlakshmi <bethlakshmi@gmail.com>
2015-11-29 12:08:15 -0500

    pep8

--- expo/gbe/views.py
+++ expo/gbe/views.py
@@ -285,8 +285,8 @@ def edit_troupe(request, troupe_id=None):
         troupe = Troupe()
 
     if (troupe_id > 0 and
-        request.user and
-        troupe.contact != request.user.profile):
+            request.user and
+            troupe.contact != request.user.profile):
         return HttpResponseRedirect(reverse('troupe_view',
                                             urlconf='gbe.urls',
                                             args=[str(troupe_id)]))
@@ -2420,7 +2420,8 @@ def conference_volunteer(request):
             else:
                 form.fields['how_volunteer'] = forms.ChoiceField(
                     choices=conference_participation_types)
-            form.fields['how_volunteer'].widget.attrs['class'] = 'how_volunteer'
+            form.fields['how_volunteer'].widget.attrs['class'] = \
+                'how_volunteer'
             bid_row = {}
             bid_row['conf_item'] = aclass.presenter_bid_info
             bid_row['form'] = form
