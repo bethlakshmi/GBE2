2b6fd2bf6259f1d6696ddd11b428e733acba78d3
bethlakshmi <bethlakshmi@gmail.com>
2015-11-30 16:57:11 -0500

    Couldn't do w/out a test case
    I didn’t like leaving this w/out a test case, and the overhead of
    figuring it out seemed worth it for the long run.

--- expo/tests/gbe/test_edit_act_techinfo.py
+++ expo/tests/gbe/test_edit_act_techinfo.py
@@ -8,33 +8,47 @@ from django.test import Client
 from gbe.views import edit_act_techinfo
 import mock
 from django.contrib.auth.models import Group
-from tests.factories import gbe_factories as factories
+from tests.factories.gbe_factories import (
+    ActFactory,
+    ShowFactory,
+    PersonaFactory,
+    RoomFactory,
+)
 from tests.functions.gbe_functions import (login_as,
                                            is_login_page,
                                            is_profile_update_page,
                                            location)
+from scheduler.models import (
+    Event as sEvent,
+)
 
-
-class TestReviewProposalList(TestCase):
+class TestEditActTechInfo(TestCase):
     '''Tests for edit_act_techinfo view'''
 
     def setUp(self):
         self.factory = RequestFactory()
         self.client = Client()
-        self.performer = factories.PersonaFactory.create()
+        self.performer = PersonaFactory.create()
 
 
-'''
-# this is a little tricky...
     def test_edit_act_techinfo_authorized_user(self):
-        act = factories.ActFactory.create()
-        show = factories.ShowFactory.create()
+        act = ActFactory.create()
+        show = ShowFactory.create()
+        room = conf.Room(name="Theater", capacity=200, overbook_size=200)
+        room.save()
         show.acts.add(act)
-        request= self.factory.get('act/changestate/%d' %act.pk)
+        show_event = sEvent(eventitem=show,
+                            starttime="2015-01-01 01:01Z",
+                            max_volunteer=0)
+        show_event.save()
+        show_event.set_location(room)
+        
+        request= self.factory.get('/acttechinfo/edit/%d' %act.pk)
         request.user =  act.performer.performer_profile.user_object
-        mock_get_shows = mock.MagicMock(return_value = [])
+        request.session = {'cms_admin_site':1}
+        mock_get_shows = mock.MagicMock(return_value = [show_event])
         with mock.patch ('scheduler.models.ActItem.get_scheduled_shows',
                           mock_get_shows):
             response = edit_act_techinfo(request, act.pk)
-        nt.assert_equal(response.status_code, 302)
-'''
+        nt.assert_equal(response.status_code, 200)
+        nt.assert_false('In lists of choices:' in response.content)
