d423c5bb772d0e34a124cbc5408065aad2ab69e8
bethlakshmi <bethlakshmi@gmail.com>
2015-11-30 17:01:14 -0500

    pep8 for test

--- expo/tests/gbe/test_edit_act_techinfo.py
+++ expo/tests/gbe/test_edit_act_techinfo.py
@@ -12,7 +12,6 @@ from tests.factories.gbe_factories import (
     ActFactory,
     ShowFactory,
     PersonaFactory,
-    RoomFactory,
 )
 from tests.functions.gbe_functions import (login_as,
                                            is_login_page,
@@ -22,6 +21,7 @@ from scheduler.models import (
     Event as sEvent,
 )
 
+
 class TestEditActTechInfo(TestCase):
     '''Tests for edit_act_techinfo view'''
 
@@ -29,7 +29,6 @@ class TestEditActTechInfo(TestCase):
         self.factory = RequestFactory()
         self.client = Client()
         self.performer = PersonaFactory.create()
-
 
     def test_edit_act_techinfo_authorized_user(self):
         act = ActFactory.create()
@@ -42,13 +41,13 @@ class TestEditActTechInfo(TestCase):
                             max_volunteer=0)
         show_event.save()
         show_event.set_location(room)
-        
-        request= self.factory.get('/acttechinfo/edit/%d' %act.pk)
-        request.user =  act.performer.performer_profile.user_object
-        request.session = {'cms_admin_site':1}
-        mock_get_shows = mock.MagicMock(return_value = [show_event])
-        with mock.patch ('scheduler.models.ActItem.get_scheduled_shows',
-                          mock_get_shows):
+
+        request = self.factory.get('/acttechinfo/edit/%d' % act.pk)
+        request.user = act.performer.performer_profile.user_object
+        request.session = {'cms_admin_site': 1}
+        mock_get_shows = mock.MagicMock(return_value=[show_event])
+        with mock.patch('scheduler.models.ActItem.get_scheduled_shows',
+                        mock_get_shows):
             response = edit_act_techinfo(request, act.pk)
         nt.assert_equal(response.status_code, 200)
         nt.assert_false('In lists of choices:' in response.content)
