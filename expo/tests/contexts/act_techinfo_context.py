34f3b72946bbfd67b09962e2982c55ec9ddf11ab
Jon Kiparsky <jon@kiparsky.net>
2016-02-19 00:27:25 -0500

    GBE-722: Changes suggested in code review

--- expo/tests/contexts/act_techinfo_context.py
+++ expo/tests/contexts/act_techinfo_context.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 from tests.factories.gbe_factories import (
     ActFactory,
     ConferenceFactory,
@@ -44,3 +45,53 @@ class ActTechInfoContext():
         ResourceAllocationFactory(
             event=sched_event,
             resource=ActResourceFactory(_item=self.act.actitem_ptr))
+||||||| merged common ancestors
+=======
+from tests.factories.gbe_factories import (
+    ActFactory,
+    ConferenceFactory,
+    CueInfoFactory,
+    PersonaFactory,
+    RoomFactory,
+    ShowFactory,
+)
+from tests.factories.scheduler_factories import (
+    ActResourceFactory,
+    LocationFactory,
+    ResourceAllocationFactory,
+    SchedEventFactory,
+)
+
+class ActTechInfoContext():
+    def __init__(self,
+                 performer=None,
+                 act=None,
+                 show=None,
+                 conference=None,
+                 room_name=None,
+                 cue_count=1):
+        self.conference = conference or ConferenceFactory()
+        self.performer = performer or PersonaFactory()
+        self.act = act or ActFactory(performer=self.performer,
+                                     conference=self.conference,
+                                     accepted=3)
+        self.tech = self.act.tech
+        self.audio = self.tech.audio
+        self.lighting = self.tech.lighting
+        self.stage = self.tech.stage
+        for i in range(cue_count):
+            CueInfoFactory.create(techinfo=self.tech,
+                                  cue_sequence=i)
+        self.show = show or ShowFactory(conference=self.conference)
+                # schedule the show
+        sched_event = SchedEventFactory(eventitem=self.show.eventitem_ptr)
+        room_name = room_name or "Dining Room"
+        self.room = RoomFactory(name=room_name)
+        ResourceAllocationFactory(
+            event=sched_event,
+            resource=LocationFactory(_item=self.room.locationitem_ptr))
+                # schedule the act into the show
+        ResourceAllocationFactory(
+            event=sched_event,
+            resource=ActResourceFactory(_item=self.act.actitem_ptr))
+>>>>>>> Stashed changes
--- expo/tests/contexts/volunteer_context.py
+++ expo/tests/contexts/volunteer_context.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 from tests.factories.gbe_factories import (
     GenericEventFactory,
     ProfileFactory,
@@ -42,3 +43,49 @@ class VolunteerContext():
                                                     event=self.sched_event)
         EventContainerFactory(parent_event=self.sched_event,
                               child_event=self.opp_event)
+||||||| merged common ancestors
+=======
+from tests.factories.gbe_factories import (
+    GenericEventFactory,
+    ProfileFactory,
+    ShowFactory,
+    VolunteerFactory,
+)
+from tests.factories.scheduler_factories import (
+    EventContainerFactory,
+    ResourceAllocationFactory,
+    SchedEventFactory,
+    WorkerFactory,
+)
+
+
+class VolunteerContext():
+    def __init__(self,
+                 profile=None,
+                 bid=None,
+                 event=None,
+                 opportunity=None,
+                 role=None):
+        if bid is False:
+            self.profile = profile or ProfileFactory()
+            self.bid = None
+        elif bid:
+            self.bid = bid
+            self.profile = self.bid.profile
+        else:
+            self.bid = VolunteerFactory()
+            self.profile = self.bid.profile
+        self.opportunity = opportunity or GenericEventFactory()
+        self.event = event or ShowFactory()
+        self.role = role or "Volunteer"
+        self.sched_event = SchedEventFactory(
+            eventitem=self.event.eventitem_ptr)
+        self.opp_event = SchedEventFactory(
+            eventitem=self.opportunity.eventitem_ptr)
+        self.worker = WorkerFactory(_item=self.profile.workeritem,
+                                    role=self.role)
+        self.allocation = ResourceAllocationFactory(resource=self.worker,
+                                                    event=self.sched_event)
+        EventContainerFactory(parent_event=self.sched_event,
+                              child_event=self.opp_event)
+>>>>>>> Stashed changes
