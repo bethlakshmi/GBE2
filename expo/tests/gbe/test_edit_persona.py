3849ae501bd948fe5307d56d670b9bf365d81e2c
Jon Kiparsky <jon@kiparsky.net>
2016-03-05 01:24:59 -0500

    GBE-730: Completed tests for edit_profile

--- expo/tests/gbe/test_edit_persona.py
+++ expo/tests/gbe/test_edit_persona.py
@@ -6,6 +6,7 @@ from unittest import TestCase
 from django.test.client import RequestFactory
 from django.test import Client
 from gbe.views import edit_persona
+from gbe.models import Persona
 from tests.factories.gbe_factories import (
     PersonaFactory,
     ProfileFactory,
@@ -22,7 +23,7 @@ class TestEditPersona(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
         self.client = Client()
-        self.troupe_string = 'Tell Us About Your Stage Persona'
+        self.expected_string = 'Tell Us About Your Stage Persona'
 
     def test_edit_persona(self):
         '''edit_troupe view, create flow
@@ -45,7 +46,7 @@ class TestEditPersona(TestCase):
         login_as(contact.performer_profile, self)
         response = edit_persona(request, contact.resourceitem_id)
         self.assertEqual(response.status_code, 200)
-        self.assertTrue(self.troupe_string in response.content)
+        self.assertTrue(self.expected_string in response.content)
 
     @nt.raises(PermissionDenied)
     def test_wrong_profile(self):
@@ -58,3 +59,49 @@ class TestEditPersona(TestCase):
         login_as(viewer, self)
         request.session = {'cms_admin_site': 1}
         edit_persona(request, persona.pk)
+
+    def test_edit_persona_change_stage_name(self):
+        persona = PersonaFactory()
+        login_as(persona.performer_profile, self)
+        old_name = persona.name
+        new_name = "Fifi"
+        urlstring = '/persona/edit/%d' % persona.resourceitem_id
+        request = self.factory.post(
+            urlstring,
+            data={'performer_profile': persona.performer_profile.pk,
+                  'contact': persona.performer_profile.pk,
+                  'name': new_name,
+                  'homepage': persona.homepage,
+                  'bio': "bio",
+                  'experience': 1,
+                  'awards': "many",}
+        )
+        request.session = {'cms_admin_site': 1}
+        request.user = persona.performer_profile.user_object
+        response = edit_persona(request, persona.resourceitem_id)
+        persona_reloaded = Persona.objects.get(pk=persona.pk)
+        nt.assert_equal(persona_reloaded.name, new_name)
+
+
+    def test_edit_persona_invalid_post(self):
+        persona = PersonaFactory()
+        login_as(persona.performer_profile, self)
+        old_name = persona.name
+        new_name = "Fifi"
+        urlstring = '/persona/edit/%d' % persona.resourceitem_id
+        request = self.factory.post(
+            urlstring,
+            data={'performer_profile': persona.pk,
+                  'contact': persona.pk,
+                  'name': new_name,
+                  'homepage': persona.homepage,
+                  'bio': "bio",
+                  'experience': 1,
+                  'awards': "many",}
+        )
+        request.session = {'cms_admin_site': 1}
+        request.user = persona.performer_profile.user_object
+        response = edit_persona(request, persona.resourceitem_id)
+        nt.assert_true(self.expected_string in response.content)
+        persona_reloaded = Persona.objects.get(pk=persona.pk)
+        nt.assert_equal(persona_reloaded.name, old_name)
