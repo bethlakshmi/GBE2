259a3a2ab2d4b0ccadc3e1b0fd8bbbb886652993
Jon Kiparsky <jon@kiparsky.net>
2016-03-05 15:50:35 -0500

    GBE-730: Tests for bid_class, conference_volunteer

--- expo/tests/gbe/test_bid_class.py
+++ expo/tests/gbe/test_bid_class.py
@@ -4,7 +4,11 @@ from unittest import TestCase
 from django.test.client import RequestFactory
 from django.test import Client
 from gbe.views import bid_class
-from tests.factories import gbe_factories as factories
+from tests.factories.gbe_factories import (
+    ConferenceFactory,
+    PersonaFactory,
+    ProfileFactory,
+)
 from tests.functions.gbe_functions import (
     location,
     login_as
@@ -17,8 +21,9 @@ class TestEditClass(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
         self.client = Client()
-        self.performer = factories.PersonaFactory.create()
-        self.teacher = factories.PersonaFactory.create()
+        self.performer = PersonaFactory.create()
+        self.teacher = PersonaFactory.create()
+        self.conference = ConferenceFactory(accepting_bids=True)
 
     def get_class_form(self):
         return {'teacher': self.teacher.pk,
@@ -28,12 +33,13 @@ class TestEditClass(TestCase):
                 'maximum_enrollment': 20,
                 'fee': 0,
                 'schedule_constraints': ['0'],
+                'conference': self.conference
                 }
 
     def test_bid_class_no_personae(self):
         '''class_bid, when profile has no personae,
         should redirect to persona_create'''
-        profile = factories.ProfileFactory.create()
+        profile = ProfileFactory.create()
         request = self.factory.get('/class/create')
         request.user = profile.user_object
         response = bid_class(request)
@@ -64,6 +70,41 @@ class TestEditClass(TestCase):
         response = bid_class(request)
         nt.assert_equal(response.status_code, 302)
         nt.assert_equal(location(response), '/gbe')
+
+
+    def test_class_bid_post_with_submit(self):
+        '''class_bid, not submitting and no other problems,
+        should redirect to home'''
+        request = self.client.get('/class/create',
+                                  follow=True)
+        request.user = self.performer.performer_profile.user_object
+        login_as(request.user, self)
+        request.method = 'POST'
+        request.POST = {}
+        request.POST.update(self.get_class_form())
+        request.POST.update({'submit':1})
+        request.session = {'cms_admin_site':1}
+        response = bid_class(request)
+        nt.assert_equal(response.status_code, 302)
+        nt.assert_equal(location(response), '/gbe')
+
+
+    def test_class_bid_post_invalid(self):
+        '''class_bid, not submitting and no other problems,
+        should redirect to home'''
+        request = self.client.get('/class/create',
+                                  follow=True)
+        request.user = self.performer.performer_profile.user_object
+        login_as(request.user, self)
+        request.method = 'POST'
+        request.POST = {}
+        request.POST.update(self.get_class_form())
+        request.session = {'cms_admin_site':1}
+        response = bid_class(request)
+
+        nt.assert_true(True)
+
+
 
     def test_class_bid_not_post(self):
         '''act_bid, not post, should take us to bid process'''
--- expo/tests/gbe/test_conference_volunteer.py
+++ expo/tests/gbe/test_conference_volunteer.py
@@ -9,11 +9,13 @@ from gbe.views import conference_volunteer
 from tests.factories.gbe_factories import (
     ClassFactory,
     ClassProposalFactory,
+    ConferenceVolunteerFactory,
     PersonaFactory,
     ProfileFactory,
     UserFactory,
 )
-
+from gbe.models import ClassProposal
+from gbetext import conf_volunteer_save_error
 
 class TestConferenceVolunteer(TestCase):
     '''Tests for conference_volunteer view'''
@@ -26,11 +28,15 @@ class TestConferenceVolunteer(TestCase):
     def get_class_form(self):
         return {'name': 'someone@host.com',
                 'title': 'some class name',
-                'proposal': 'some class description'
+                'proposal': 'some class description',
+                'presenter': 1,
+                'bid': 1,
+                'how_volunteer': 1
                 }
 
-    def test_conference_volunteer_authorized_user(self):
-        proposal = ClassProposalFactory()
+    def test_conference_volunteer_no_visible_class_proposals(self):
+        ClassProposal.objects.all().delete()
+        proposal = ClassProposalFactory(display=False)
         request = self.factory.get('conference/volunteer/')
         request.user = ProfileFactory().user_object
         request.session = {'cms_admin_site': 1}
@@ -39,8 +45,9 @@ class TestConferenceVolunteer(TestCase):
 
 
 
-    def test_conference_volunteer_no_persona(self):
-        proposal = ClassProposalFactory(display=True)
+    def test_conference_volunteer_no_profile(self):
+        ClassProposal.objects.all().delete()
+        proposal = ClassProposalFactory(display=False)
         request = self.factory.get('conference/volunteer/')
         request.user = UserFactory()
         request.session = {'cms_admin_site': 1}
@@ -49,13 +56,76 @@ class TestConferenceVolunteer(TestCase):
 
 
 
+    def test_conference_volunteer_post_offer_form_invalid(self):
+        proposal = ClassProposalFactory(display=True)
+        persona = PersonaFactory()
+        data=self.get_class_form()
+        data["%d-volunteering" % proposal.id] = 1
+        request = self.factory.post('conference/volunteer/',
+                                    data=data)
 
-    # def test_conference_volunteer_no_personae(self):
-    #     '''class_bid, when profile has no personae,
-    #     should redirect to persona_create'''
-    #     profile = ProfileFactory()
-    #     request = self.factory.get('conference/volunteer/')
-    #     request.user = profile.user_object
-    #     request.session = {'cms_admin_site': 1}
-    #     response = conference_volunteer(request)
-    #     nt.assert_equal(response.status_code, 302)
+        request.user = persona.contact.user_object
+        request.session = {'cms_admin_site': 1}
+        response = conference_volunteer(request)
+
+        nt.assert_equal(response.status_code, 200)
+        nt.assert_true(conf_volunteer_save_error in response.content)
+
+    def test_conference_volunteer_post_offer_form_valid(self):
+        proposal = ClassProposalFactory(display=True)
+        persona = PersonaFactory()
+        data=self.get_class_form()
+        data["%d-volunteering" % proposal.id] = 1
+        data["%d-bid" % proposal.pk]=proposal.id
+        data['%d-how_volunteer' % proposal.pk] = 'Any of the Above'
+        data['%d-presenter' % proposal.pk] = persona.pk
+        request = self.factory.post('conference/volunteer/',
+                                    data=data)
+        request.user = persona.contact.user_object
+        request.session = {'cms_admin_site': 1}
+
+        response = conference_volunteer(request)
+        nt.assert_equal(response.status_code, 302)
+
+    def test_conference_volunteer_post_offer_existing_volunteer(self):
+        proposal = ClassProposalFactory(display=True)
+        persona = ConferenceVolunteerFactory(
+            bid=proposal).presenter
+
+        data=self.get_class_form()
+        data["%d-volunteering" % proposal.id] = 1
+        data["%d-bid" % proposal.pk]=proposal.id
+        data['%d-how_volunteer' % proposal.pk] = 'Any of the Above'
+        data['%d-presenter' % proposal.pk] = persona.pk
+        request = self.factory.post('conference/volunteer/',
+                                    data=data)
+        request.user = persona.contact.user_object
+        request.session = {'cms_admin_site': 1}
+        response = conference_volunteer(request)
+        nt.assert_equal(response.status_code, 302)
+
+
+    def test_conference_volunteer_get(self):
+        proposal = ClassProposalFactory(display=True)
+        ClassProposalFactory(display=True,
+                             type="Panel")
+        ClassProposalFactory(display=True,
+                             type="Shoe")
+        persona = ConferenceVolunteerFactory(
+            bid=proposal).presenter
+
+        request = self.factory.get('conference/volunteer/')
+        request.user = persona.contact.user_object
+        request.session = {'cms_admin_site': 1}
+
+        response = conference_volunteer(request)
+        nt.assert_equal(response.status_code, 200)
+
+
+    def test_conference_volunteer_no_persona(self):
+        proposal = ClassProposalFactory(display=True)
+        request = self.factory.get('conference/volunteer/')
+        request.user = ProfileFactory().user_object
+        request.session = {'cms_admin_site': 1}
+        response = conference_volunteer(request)
+        nt.assert_equal(response.status_code, 302)
