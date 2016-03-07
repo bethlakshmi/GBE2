67ec675ae3c9745096c45b1c0c311879f0326467
Jon Kiparsky <jon@kiparsky.net>
2016-03-06 23:13:22 -0500

    GBE-730: Coverage for view_volunteer

--- expo/tests/gbe/test_view_volunteer.py
+++ expo/tests/gbe/test_view_volunteer.py
@@ -1,12 +1,15 @@
-import gbe.models as conf
 import nose.tools as nt
+from django.core.exceptions import PermissionDenied
 from unittest import TestCase
 from django.test.client import RequestFactory
 from django.test import Client
 from gbe.views import view_volunteer
-from tests.factories import gbe_factories as factories
+from tests.factories.gbe_factories import (
+    PersonaFactory,
+    ProfileFactory,
+    VolunteerFactory
+)
 import mock
-import gbe.ticketing_idd_interface
 
 
 class TestViewVolunteer(TestCase):
@@ -15,12 +18,23 @@ class TestViewVolunteer(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
         self.client = Client()
-        self.performer = factories.PersonaFactory.create()
+        self.performer = PersonaFactory.create()
 
     def test_view_act_all_well(self):
-        volunteer = factories.VolunteerFactory.create()
+        volunteer = VolunteerFactory.create()
         request = self.factory.get('volunteer/view/%d' % volunteer.pk)
         request.user = volunteer.profile.user_object
+        request.session = {'cms_admin_site': 1}
+        response = view_volunteer(request, volunteer.pk)
+        test_string = 'Submitted proposals cannot be modified'
+        nt.assert_equal(response.status_code, 200)
+        nt.assert_true(test_string in response.content)
+
+    @nt.raises(PermissionDenied)
+    def test_view_act_wrong_profile(self):
+        volunteer = VolunteerFactory.create()
+        request = self.factory.get('volunteer/view/%d' % volunteer.pk)
+        request.user = ProfileFactory().user_object
         request.session = {'cms_admin_site': 1}
         response = view_volunteer(request, volunteer.pk)
         test_string = 'Submitted proposals cannot be modified'
