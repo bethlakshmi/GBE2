f34c6df591e3953907bf23d8b7727db3f8c875e7
Jon Kiparsky <jon@kiparsky.net>
2016-03-08 00:11:47 -0500

    GBE-730: coverage for create_event

--- expo/tests/gbe/test_create_event.py
+++ expo/tests/gbe/test_create_event.py
@@ -7,8 +7,14 @@ from django.test.client import RequestFactory
 from django.test import Client
 from gbe.views import create_event
 from django.contrib.auth.models import Group
-from tests.factories import gbe_factories as factories
-
+from tests.factories.gbe_factories import (
+    PersonaFactory,
+    ProfileFactory,
+)
+from tests.functions.gbe_functions import (
+    current_conference,
+    grant_privilege,
+)
 
 class TestCreateEvent(TestCase):
     '''Tests for create_event view'''
@@ -16,15 +22,15 @@ class TestCreateEvent(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
         self.client = Client()
-        self.performer = factories.PersonaFactory.create()
-        group, created = Group.objects.get_or_create(name='Scheduling Mavens')
-        self.privileged_user = factories.ProfileFactory.create().user_object
-        self.privileged_user.groups.add(group)
+        self.performer = PersonaFactory.create()
+        self.privileged_user = ProfileFactory.create().user_object
+        grant_privilege(self.privileged_user, 'Scheduling Mavens')
+        current_conference()
 
     def test_authorized_user_can_access(self):
         request = self.factory.get('create_event/Show')
         request.user = self.privileged_user
-        request.session = {'cms_admin_site':1}
+        request.session = {'cms_admin_site': 1}
         response = create_event(request, 'Show')
         nt.assert_equal(response.status_code, 200)
 
@@ -39,3 +45,16 @@ class TestCreateEvent(TestCase):
         request.POST['duration'] = duration
         create_event(request, 'Show')
         nt.assert_true(conf.Show.objects.filter(title=title).count() > 0)
+
+
+    def test_invalid_form(self):
+        request = self.factory.post('create_event/Show')
+        request.user = self.privileged_user
+        title = 'The Little Show'
+        request.POST['title'] = title
+        description = 'Really Big Show'
+        request.POST['description'] = description
+        request.session = {'cms_admin_site': 1}
+        duration = '2:00:00'
+        create_event(request, 'Show')
+        nt.assert_false(conf.Show.objects.filter(title=title).exists())
