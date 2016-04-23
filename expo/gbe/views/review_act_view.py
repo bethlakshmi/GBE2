dede5323a6de5d8fab0d1f536399f7400d01ff8a
Jon Kiparsky <jon@kiparsky.net>
2016-04-23 14:35:10 -0400

    GBE-754: Fix and test for review_act regression

--- expo/gbe/views/review_act_view.py
+++ expo/gbe/views/review_act_view.py
@@ -64,11 +64,10 @@ def ReviewActView(request, act_id):
     '''
     if user has previously reviewed the act, provide their review for update
     '''
-    try:
-        bid_eval = BidEvaluation.objects.filter(
-            bid_id=act_id,
-            evaluator_id=reviewer.resourceitem_id).first()
-    except:
+    bid_eval = BidEvaluation.objects.filter(
+        bid_id=act_id,
+        evaluator_id=reviewer.resourceitem_id).first()
+    if bid_eval is None:
         bid_eval = BidEvaluation(evaluator=reviewer, bid=act)
 
     # show act info and inputs for review
