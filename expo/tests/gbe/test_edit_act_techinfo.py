61c4d6fc7fd821de71e6a834dde67e0906bdde92
bethlakshmi <bethlakshmi@gmail.com>
2016-05-27 13:09:25 -0400

    New test passing

--- expo/tests/gbe/test_edit_act_techinfo.py
+++ expo/tests/gbe/test_edit_act_techinfo.py
@@ -28,6 +28,55 @@ class TestEditActTechInfo(TestCase):
         self.client = Client()
         self.performer = PersonaFactory()
 
+    def get_full_post(self, rehearsal, techinfo, show):
+        data = {
+            'show': show.title,
+            'rehearsal': str(rehearsal.pk),
+            'lighting_info-notes': 'lighting notes',
+            'lighting_info-costume': 'costume description',
+            'lighting_info-specific_needs': 'lighting specific needs',
+            'audio_info-track_title': 'track title',
+            'audio_info-track_artist': 'artist',
+            'audio_info-track_duration': '00:03:30',
+            'audio_info-need_mic': 'checked',
+            'audio_info-own_mic': 'checked',
+            'audio_info-notes': 'audio notes',
+            'audio_info-confirm_no_music': 'checked',
+            'stage_info-act_duration': '00:04:10',
+            'stage_info-intro_text': 'intro act',
+            'stage_info-set_props': 'checked',
+            'stage_info-clear_props': 'checked',
+            'stage_info-notes': 'notes',
+            'stage_info-set_props': 'checked',
+            'stage_info-set_props': 'checked',
+            'stage_info-set_props': 'checked',
+            'cue0-cue_off_of': 'Start of music',
+            'cue0-follow_spot': 'Blue',
+            'cue0-center_spot': 'ON',
+            'cue0-backlight': 'ON',
+            'cue0-cyc_color': 'White',
+            'cue0-wash': 'Blue',
+            'cue0-sound_note': 'sound note',
+            'cue0-techinfo': techinfo.pk,
+            'cue1-cue_off_of': 'cue note',
+            'cue1-follow_spot': 'Pink',
+            'cue1-center_spot': 'OFF',
+            'cue1-backlight': 'OFF',
+            'cue1-cyc_color': 'Green',
+            'cue1-wash': 'Green',
+            'cue1-sound_note': 'sound note',
+            'cue1-techinfo': techinfo.pk,
+            'cue2-cue_off_of': 'cue note',
+            'cue2-follow_spot': 'Red',
+            'cue2-center_spot': 'ON',
+            'cue2-backlight': 'ON',
+            'cue2-cyc_color': 'Red',
+            'cue2-wash': 'Red',
+            'cue2-sound_note': 'sound note',
+            'cue2-techinfo': techinfo.pk,
+        }
+        return data
+
     def test_edit_act_techinfo_unauthorized_user(self):
         context = ActTechInfoContext()
         url = reverse('act_techinfo_edit',
@@ -75,3 +124,21 @@ class TestEditActTechInfo(TestCase):
         response = self.client.post(url, {})
         self.assertEqual(response.status_code, 200)
         self.assertTrue("Cue Sheet Instructions" in response.content)
+
+    def test_edit_act_techinfo_authorized_user_post_complete_form(self):
+        context = ActTechInfoContext(schedule_rehearsal=True)
+        another_rehearsal = context._schedule_rehearsal(context.sched_event)
+        url = reverse('act_techinfo_edit',
+                      urlconf='gbe.urls',
+                      args=[context.act.pk])
+        login_as(context.performer.contact, self)
+        response = self.client.post(
+            url,
+            data=self.get_full_post(
+                another_rehearsal,
+                context.act.tech,
+                context.show))
+        self.assertRedirects(response, reverse('home', urlconf='gbe.urls'))
+        self.assertEqual(len(context.act.get_scheduled_rehearsals()), 1)
+        self.assertEqual(context.act.get_scheduled_rehearsals()[0],
+                         another_rehearsal)
