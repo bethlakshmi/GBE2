d54f827381dc9da8b73d123f11bf8dd598df9e0c
bethlakshmi <bethlakshmi@gmail.com>
2016-07-09 20:21:51 -0400

    Basics are now working with bootstrap
    refactored to avoid conflict in alerts in gbe vs. alerts that fit the
    new momentary messages.

--- expo/gbe/views/bid_class_view.py
+++ expo/gbe/views/bid_class_view.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.forms.models import ModelChoiceField
@@ -13,7 +14,8 @@ from gbe.forms import (
 from gbe.models import (
     Class,
     Conference,
-    Persona
+    Persona,
+    UserMessage
 )
 from gbe.functions import validate_profile
 
@@ -45,10 +47,23 @@ def BidClassView(request):
         If this is a draft, only a few fields are needed, use a form with fewer
         required fields (same model)
         '''
+
         if 'submit' in request.POST.keys():
             form = ClassBidForm(request.POST)
+            user_message = UserMessage.objects.get_or_create(
+                view='BidClassView',
+                code="SUBMIT_SUCCESS",
+                defaults={
+                    'summary': "Class Draft Success",
+                    'description': "Your class was sucessfully submitted"})
         else:
             form = ClassBidDraftForm(request.POST)
+            user_message = UserMessage.objects.get_or_create(
+                view='BidClassView',
+                code="SUBMIT_DRAFT",
+                defaults={
+                    'summary': "Class Draft Success",
+                    'description': "Your draft was sucessfully submitted"})
 
         if form.is_valid():
             conference = Conference.objects.filter(accepting_bids=True).first()
@@ -60,8 +75,6 @@ def BidClassView(request):
                     new_class.submitted = True
                     new_class.conference = conference
                     new_class.save()
-                    return HttpResponseRedirect(reverse('home',
-                                                        urlconf='gbe.urls'))
                 else:
                     error_string = 'Cannot submit, class is not complete'
                     return render(request,
@@ -71,7 +84,7 @@ def BidClassView(request):
                                    'view_title': view_title,
                                    'draft_fields': draft_fields,
                                    'errors': [error_string]})
-                    new_class.save()
+            messages.success(request, user_message[0].description)
             return HttpResponseRedirect(reverse('home', urlconf='gbe.urls'))
         else:
             fields, requiredsub = Class().get_bid_fields
