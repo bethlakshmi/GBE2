c0425b071ca12e93f9b4a89399ca0aa040fe55f9
bethlakshmi <bethlakshmi@gmail.com>
2016-07-23 23:01:12 -0400

    goodbye fixture
    Fixture was broken.
    Rather than rehab it, I removed it.

--- expo/tests/gbe/test_get_tickets.py
+++ expo/tests/gbe/test_get_tickets.py
@@ -2,21 +2,27 @@ import nose.tools as nt
 from django.test import TestCase
 from django.test.client import RequestFactory
 from django.test import Client
-from gbe.models import GenericEvent, Show, Class
-from ticketing.models import TicketItem
 import mock
+from tests.factories.gbe_factories import (
+    ClassFactory,
+    GenericEventFactory,
+    ShowFactory
+)
+from tests.factories.ticketing_factories import (
+    BrownPaperEventsFactory,
+    TicketItemFactory
+)
 
 
 class TestGetTickets(TestCase):
     '''Tests for edit_event view'''
 
     # Fixture to create some rooms, location items, and resource items
-    fixtures = ['tests/fixtures/tickets_to_events.json']
 
     def test_get_tickets_for_volunteer_opp(self):
         '''should get no tickets, volunteer opportunities are free
         '''
-        event = GenericEvent.objects.get(pk=12)
+        event = GenericEventFactory()
         tickets = event.get_tickets()
 
         self.assertEqual(tickets, [])
@@ -24,7 +30,14 @@ class TestGetTickets(TestCase):
     def test_get_tickets_for_master_class(self):
         '''get the one ticket that is active for the Master Class
         '''
-        event = GenericEvent.objects.get(pk=4)
+        event = GenericEventFactory(
+            type='Master')
+        bpt_event = BrownPaperEventsFactory(conference=event.conference)
+        bpt_event.linked_events.add(event)
+        bpt_event.save()
+        TicketItemFactory(bpt_event=bpt_event,
+                          active=True,
+                          title="Master Class 2017")
         tickets = event.get_tickets()
 
         self.assertEqual(len(tickets), 1)
@@ -33,7 +46,15 @@ class TestGetTickets(TestCase):
     def test_get_tickets_for_special_event(self):
         '''get the one ticket that is active for all except master classes
         '''
-        event = GenericEvent.objects.get(pk=8)
+        event = GenericEventFactory(
+            type='Special')
+        bpt_event = BrownPaperEventsFactory(
+            conference=event.conference,
+            include_most=True)
+        TicketItemFactory(bpt_event=bpt_event,
+                          active=True,
+                          title="The Whole Shebang 2016")
+
         tickets = event.get_tickets()
 
         self.assertEqual(len(tickets), 1)
@@ -42,11 +63,20 @@ class TestGetTickets(TestCase):
     def test_get_tickets_for_class(self):
         '''get one ticket for everything but master, and one for classes
         '''
-        event = Class.objects.get(pk=2)
+        event = ClassFactory()
+        ws_bpt_event = BrownPaperEventsFactory(
+            conference=event.conference,
+            include_most=True)
+        sch_bpt_event = BrownPaperEventsFactory(
+            conference=event.conference,
+            include_conference=True)
+        whole_shebang = TicketItemFactory(bpt_event=ws_bpt_event,
+                          active=True,
+                          title="The Whole Shebang 2016")
+        scholar = TicketItemFactory(bpt_event=sch_bpt_event,
+                          active=True,
+                          title="The Scholar 2016")
         tickets = event.get_tickets()
-
-        whole_shebang = TicketItem.objects.get(pk=1)
-        scholar = TicketItem.objects.get(pk=3)
 
         self.assertEqual(len(tickets), 2)
         self.assertIn(whole_shebang, tickets)
@@ -55,7 +85,13 @@ class TestGetTickets(TestCase):
     def test_get_tickets_for_show(self):
         '''just gets 1 ticket for Whole Shebang
         '''
-        event = Show.objects.get(pk=2)
+        event = ShowFactory()
+        bpt_event = BrownPaperEventsFactory(
+            conference=event.conference,
+            include_most=True)
+        TicketItemFactory(bpt_event=bpt_event,
+                          active=True,
+                          title="The Whole Shebang 2016")
         tickets = event.get_tickets()
 
         self.assertEqual(len(tickets), 1)
