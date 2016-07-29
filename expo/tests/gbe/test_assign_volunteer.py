d02f0cfefb9882ef86b8dd127ef30027570eb071
bethlakshmi <bethlakshmi@gmail.com>
2016-07-29 09:39:16 -0400

    last line covered

--- expo/tests/gbe/test_assign_volunteer.py
+++ expo/tests/gbe/test_assign_volunteer.py
@@ -40,7 +40,7 @@ class TestAssignVolunteer(TestCase):
         grant_privilege(self.privileged_user, 'Volunteer Coordinator')
         grant_privilege(self.privileged_user, 'Volunteer Reviewers')
 
-    def set_basic_opportunity():
+    def set_basic_opportunity(self):
         context = StaffAreaContext()
         parent_sched = context.schedule_instance(
             starttime=datetime(2016, 2, 5, 12, 0, 0, 0, pytz.utc)
@@ -66,6 +66,20 @@ class TestAssignVolunteer(TestCase):
             'current_window': current_window,
             'volunteer': volunteer
         }
+
+    def get_basic_volunteer_to_assign(self):
+        data = self.set_basic_opportunity()
+        interest = VolunteerInterestFactory(
+            volunteer=data['volunteer'])
+        data['current_sched'].eventitem.volunteer_type = interest.interest
+        data['current_sched'].eventitem.save()
+        url = reverse(self.view_name,
+                      args=[data['volunteer'].pk],
+                      urlconf="gbe.urls")
+
+        login_as(self.privileged_user, self)
+        response = self.client.get(url)
+        return response, data
 
     def test_assign_volunteers_bad_user(self):
         ''' user does not have Volunteer Coordinator, permission is denied'''
@@ -104,52 +118,25 @@ class TestAssignVolunteer(TestCase):
 
     def test_assign_volunteer_show_current_events(self):
         '''conference details show up'''
-        context = StaffAreaContext()
-        parent_sched = context.schedule_instance(
-            starttime=datetime(2016, 2, 5, 12, 0, 0, 0, pytz.utc)
-        )
-        current_sched = context.add_volunteer_opp()
-        current_sched.starttime = datetime(2016, 2, 5, 12, 0, 0, 0, pytz.utc)
-        current_sched.max_volunteer = 10
-        current_sched.save()
+        response, data = self.get_basic_volunteer_to_assign()
 
-        current_window = VolunteerWindowFactory(
-            day__conference=context.conference)
-
-        volunteer = VolunteerFactory(
-            conference=context.conference,
-            submitted=True)
-        interest = VolunteerInterestFactory(
-            volunteer=volunteer)
-        current_sched.eventitem.volunteer_type = interest.interest
-        current_sched.eventitem.save()
-        volunteer.available_windows.add(current_window)
-        volunteer.save()
-        url = reverse(self.view_name,
-                      args=[volunteer.pk],
-                      urlconf="gbe.urls")
-
-        login_as(self.privileged_user, self)
-        response = self.client.get(url)
-        print response.content
-        print str(current_sched.eventitem.volunteer_type)
         # event names
         nt.assert_equal(
-            response.content.count(str(parent_sched.eventitem)),
+            response.content.count(str(data['parent_sched'].eventitem)),
             1,
             msg="There should be only 1 parent event")
         nt.assert_equal(
-            response.content.count(str(current_sched.eventitem)),
+            response.content.count(str(data['current_sched'].eventitem)),
             1,
             msg="There should be 1 schedule items for current_sched.eventitem")
 
         # start and end times
         nt.assert_in(
-            current_sched.start_time.strftime("%a, %-I %p"),
+            data['current_sched'].start_time.strftime("%a, %-I %p"),
             response.content,
             msg="start time for current_sched didn't show up")
         nt.assert_in(
-            current_sched.end_time.strftime("%a, %-I %p"),
+            data['current_sched'].end_time.strftime("%a, %-I %p"),
             response.content,
             msg="end time for current_sched didn't show up")
 
@@ -171,26 +158,20 @@ class TestAssignVolunteer(TestCase):
         nt.assert_equal(
             response.content.count("Interested<br>"),
             1,
-            msg="volunteer vs. event interest did not match for 3 events")
+            msg="volunteer vs. event interest did not match for event")
         nt.assert_is_not_none(
             re.search(
                 '''<td class="bid-table">\s+N\s+</td>''',
                 response.content),
-            msg="The current and unavailable events should be not yet full")
+            msg="The current event should be not yet full")
 
     def test_no_rehearsal_in_events(self):
         '''no rehearsal data shows up'''
         # horrible setup process. Need to fix
-        context = StaffAreaContext()
-        parent_sched = context.schedule_instance(
-            starttime=datetime(2016, 2, 5, 12, 0, 0, 0, pytz.utc)
-        )
-        current_sched = context.add_volunteer_opp()
-        current_sched.max_volunteer = 10
-        current_sched.save()
+        data = self.set_basic_opportunity()
 
         rehearsal = GenericEventFactory(
-            conference=context.conference,
+            conference=data['context'].conference,
             type="Rehearsal Slot")
 
         rehearsal_slot = SchedEventFactory(
@@ -199,7 +180,7 @@ class TestAssignVolunteer(TestCase):
             max_volunteer=10)
 
         volunteer = VolunteerFactory(
-            conference=context.conference,
+            conference=data['context'].conference,
             submitted=True)
         url = reverse(self.view_name,
                       args=[volunteer.pk],
@@ -220,16 +201,10 @@ class TestAssignVolunteer(TestCase):
     def test_no_past_conference_details(self):
         '''only current conference events, and windows should be shown'''
         # horrible setup process. Need to fix
-        context = StaffAreaContext()
+        data = self.set_basic_opportunity()
         past_context = StaffAreaContext(
             conference=ConferenceFactory(
                 status='completed'))
-        parent_sched = context.schedule_instance(
-            starttime=datetime(2016, 2, 5, 12, 0, 0, 0, pytz.utc)
-        )
-        current_sched = context.add_volunteer_opp()
-        current_sched.eventitem.max_volunteer = 10
-        current_sched.eventitem.save()
 
         past_sched = past_context.schedule_instance(
             starttime=datetime(2015, 2, 25, 8, 0, 0, 0, pytz.utc))
@@ -238,19 +213,11 @@ class TestAssignVolunteer(TestCase):
         past_opp.eventitem.max_volunteer = 10
         past_opp.eventitem.save()
 
-        current_window = VolunteerWindowFactory(
-            day__conference=context.conference)
         past_window = VolunteerWindowFactory(
             day__conference=past_context.conference)
 
-        volunteer = VolunteerFactory(
-            conference=context.conference,
-            submitted=True
-            )
-        volunteer.available_windows.add(current_window)
-        volunteer.save()
         url = reverse(self.view_name,
-                      args=[volunteer.pk],
+                      args=[data['volunteer'].pk],
                       urlconf="gbe.urls")
         login_as(self.privileged_user, self)
         response = self.client.get(url)
@@ -266,32 +233,17 @@ class TestAssignVolunteer(TestCase):
 
     def test_booking_is_good(self):
         # horrible setup process. Need to fix
-        context = StaffAreaContext()
-        parent_sched = context.schedule_instance(
-            starttime=datetime(2016, 2, 5, 12, 0, 0, 0, pytz.utc)
-        )
-        current_sched = context.add_volunteer_opp()
-        current_sched.max_volunteer = 10
-        current_sched.save()
+        data = self.set_basic_opportunity()
 
         booked_sched = SchedEventFactory(
-            eventitem=current_sched.eventitem,
+            eventitem=data['current_sched'].eventitem,
             starttime=datetime(2016, 2, 6, 9, 0, 0, 0, pytz.utc),
             max_volunteer=1)
-        current_window = VolunteerWindowFactory(
-            day__conference=context.conference)
-        current_window.save()
-
-        volunteer = VolunteerFactory(
-            conference=context.conference,
-            submitted=True)
-        volunteer.available_windows.add(current_window)
-        volunteer.save()
-        context.book_volunteer(
+        data['context'].book_volunteer(
             booked_sched,
-            volunteer=volunteer.profile)
+            volunteer=data['volunteer'].profile)
         url = reverse(self.view_name,
-                      args=[volunteer.pk],
+                      args=[data['volunteer'].pk],
                       urlconf="gbe.urls")
 
         login_as(self.privileged_user, self)
@@ -323,34 +275,28 @@ class TestAssignVolunteer(TestCase):
 
     def test_assign_volunteer_show_unavailable_event(self):
         '''test how event shows when volunteer not available'''
-        context = StaffAreaContext()
-        parent_sched = context.schedule_instance(
-            starttime=datetime(2016, 2, 5, 12, 0, 0, 0, pytz.utc)
-        )
-        current_sched = context.add_volunteer_opp()
-        current_sched.max_volunteer = 10
-        current_sched.save()
+        data = self.set_basic_opportunity()
 
         unavail_sched = SchedEventFactory(
-            eventitem=current_sched.eventitem,
+            eventitem=data['current_sched'].eventitem,
             starttime=datetime(2016, 2, 7, 12, 0, 0, 0, pytz.utc),
             max_volunteer=10)
 
         unavail_window = VolunteerWindowFactory(
-            day__conference=context.conference,
+            day__conference=data['context'].conference,
             day__day=date(2016, 2, 7),
             start=time(11),
             end=time(15))
 
         volunteer = VolunteerFactory(
-            conference=context.conference,
+            conference=data['context'].conference,
             submitted=True)
         VolunteerInterestFactory(
-            volunteer=volunteer)
-        volunteer.unavailable_windows.add(unavail_window)
-        volunteer.save()
+            volunteer=data['volunteer'])
+        data['volunteer'].unavailable_windows.add(unavail_window)
+        data['volunteer'].save()
         url = reverse(self.view_name,
-                      args=[volunteer.pk],
+                      args=[data['volunteer'].pk],
                       urlconf="gbe.urls")
 
         login_as(self.privileged_user, self)
@@ -361,7 +307,7 @@ class TestAssignVolunteer(TestCase):
 
         # event names
         nt.assert_equal(
-            response.content.count(str(current_sched.eventitem)),
+            response.content.count(str(data['current_sched'].eventitem)),
             2,
             msg="There should be 2 schedule items for current_sched.eventitem")
 
@@ -391,35 +337,12 @@ class TestAssignVolunteer(TestCase):
             re.search(
                 '''<td class="bid-table">\s+N\s+</td>''',
                 response.content),
-            msg="The current and unavailable events should be not yet full")
+            msg="The unavailable event should be not yet full")
 
     def test_volunteer_details(self):
         '''volunteer details show up'''
-        context = StaffAreaContext()
-        parent_sched = context.schedule_instance(
-            starttime=datetime(2016, 2, 5, 12, 0, 0, 0, pytz.utc)
-        )
-        current_sched = context.add_volunteer_opp()
-        current_sched.max_volunteer = 10
-        current_sched.save()
-
-        current_window = VolunteerWindowFactory(
-            day__conference=context.conference)
-
-        volunteer = VolunteerFactory(
-            conference=context.conference,
-            submitted=True)
-        VolunteerInterestFactory(
-            volunteer=volunteer)
-        volunteer.available_windows.add(current_window)
-        volunteer.save()
-        url = reverse(self.view_name,
-                      args=[volunteer.pk],
-                      urlconf="gbe.urls")
-
-        login_as(self.privileged_user, self)
-        response = self.client.get(url)
-
+        response, data = self.get_basic_volunteer_to_assign()
+        volunteer = data['volunteer']
         nt.assert_equal(response.status_code, 200)
         nt.assert_true('Assign Volunteer to Opportunities' in response.content)
 
@@ -429,6 +352,32 @@ class TestAssignVolunteer(TestCase):
         nt.assert_true(
             "Security/usher - Somewhat interested" in
             response.content)
+        nt.assert_true(volunteer.opt_outs in response.content)
+        nt.assert_true(str(volunteer.pre_event) in response.content)
+        nt.assert_true(volunteer.background in response.content)
+
+    def test_post(self):
+        ''' test that the post style of request works '''
+    def test_volunteer_details(self):
+        '''volunteer details show up'''
+        data = self.set_basic_opportunity()
+        volunteer = data['volunteer']
+
+        url = reverse(self.view_name,
+                      args=[data['volunteer'].pk],
+                      urlconf="gbe.urls")
+
+        login_as(self.privileged_user, self)
+        response = self.client.post(
+            url,
+            data={'volunteer': volunteer.pk})
+
+        nt.assert_equal(response.status_code, 200)
+        nt.assert_true('Assign Volunteer to Opportunities' in response.content)
+
+        # volunteer details
+        nt.assert_true(volunteer.profile.display_name in response.content)
+        nt.assert_true(str(volunteer.number_shifts) in response.content)
         nt.assert_true(volunteer.opt_outs in response.content)
         nt.assert_true(str(volunteer.pre_event) in response.content)
         nt.assert_true(volunteer.background in response.content)
