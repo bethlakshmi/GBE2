031d1ebc46f1df838757a6513ebfd2f45fbd51ca
Jon Kiparsky <jon@kiparsky.net>
2016-08-20 13:16:45 -0400

    GBE-110: refactored review_costume_view

--- expo/gbe/views/review_costume_view.py
+++ expo/gbe/views/review_costume_view.py
@@ -1,3 +1,5 @@
+from django.views.generic import View
+from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
@@ -24,95 +26,93 @@ from gbe.models import (
 )
 
 
-@login_required
-@log_func
-def ReviewCostumeView(request, costume_id):
-    '''
-    Show a bid  which needs to be reviewed by the current user.
-    To show: display all information about the bid, and a standard
-    review form.
-    If user is not a reviewer, politely decline to show anything.
-    '''
-    reviewer = validate_perms(request, ('Costume Reviewers', ))
-    costume = get_object_or_404(
-        Costume,
-        id=costume_id
-    )
-    if not costume.is_current:
-        return HttpResponseRedirect(
-            reverse('costume_view', urlconf='gbe.urls', args=[costume_id]))
-    conference, old_bid = get_conf(costume)
-    costume_form = CostumeBidSubmitForm(instance=costume,
-                                        prefix='Costume Proposal')
-    details = CostumeDetailsSubmitForm(instance=costume)
+class ReviewCostumeView(View):
+    reviewer_permissions =  ('Costume Reviewers', )
+    coordinator_permissions = ('Costume Coordinator',)
+    performer_prefix = "The Performer"
+    bidder_prefix = "The Owner"
+    bid_prefix = "The Costume"
+    bidder_form_type = PersonaForm
 
-    performer = PersonaForm(instance=costume.performer,
-                            prefix='The Performer')
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(ReviewCostumeView, self).dispatch(*args, **kwargs)
 
-    profile = ParticipantForm(
-        instance=costume.profile,
-        prefix='The Owner',
-        initial={
-            'email': costume.profile.user_object.email,
-            'first_name': costume.profile.user_object.first_name,
-            'last_name': costume.profile.user_object.last_name})
+    def groundwork(self, request, args, kwargs):
+        self.costume_id = kwargs.get('costume_id', 0)
+        self.reviewer = validate_perms(request, self.reviewer_permissions)
+        self.costume = get_object_or_404(
+            Costume,
+            id=self.costume_id
+        )
+        self.conference, self.old_bid = get_conf(self.costume)
+        self.costume_form = CostumeBidSubmitForm(instance=self.costume,
+                                                 prefix=self.bid_prefix)
+        self.details = CostumeDetailsSubmitForm(instance=self.costume)
+        self.performer = self.bidder_form_type(instance=self.costume.performer,
+                                     prefix=self.performer_prefix)
+        if validate_perms(request, self.coordinator_permissions, require=False):
+            self.actionform = BidStateChangeForm(instance=self.costume)
+            self.actionURL = reverse('costume_changestate',
+                                urlconf='gbe.urls',
+                                args=[self.costume_id])
+        else:
+                self.actionform = False
+                self.actionURL = False
 
-    if validate_perms(request, ('Costume Coordinator',), require=False):
-        actionform = BidStateChangeForm(instance=costume)
-        actionURL = reverse('costume_changestate',
-                            urlconf='gbe.urls',
-                            args=[costume_id])
-    else:
-            actionform = False
-            actionURL = False
+        self.profile = ParticipantForm(
+            instance=self.costume.profile,
+            prefix=self.bidder_prefix,
+            initial={
+                'email': self.costume.profile.user_object.email,
+                'first_name': self.costume.profile.user_object.first_name,
+                'last_name': self.costume.profile.user_object.last_name})
 
-    '''
-    if user has previously reviewed the act, provide their review for update
-    '''
-    try:
-        bid_eval = BidEvaluation.objects.filter(
-            bid_id=costume_id,
-            evaluator_id=reviewer.resourceitem_id)[0]
-    except:
-        bid_eval = BidEvaluation(evaluator=reviewer, bid=costume)
 
-    # show costume info and inputs for review
-    if request.method == 'POST':
-        form = BidEvaluationForm(request.POST, instance=bid_eval)
-        if form.is_valid():
+        self.bid_eval = BidEvaluation.objects.filter(
+                bid_id=self.costume_id,
+                evaluator_id=self.reviewer.resourceitem_id).first()
+        if self.bid_eval is None:
+            bid_eval = BidEvaluation(evaluator=self.reviewer, bid=self.costume)
+        self.form = BidEvaluationForm(instance=self.bid_eval)
+
+    def bid_review_response(self, request):
+        return render(request, 'gbe/bid_review.tmpl',
+                      {'readonlyform': [
+                          self.costume_form,
+                          self.details,
+                          self.performer,
+                          self.profile],
+                       'reviewer': self.reviewer,
+                       'form': self.form,
+                       'actionform': self.actionform,
+                       'actionURL': self.actionURL,
+                       'conference': self.conference,
+                       'old_bid': self.old_bid,
+                      })
+
+
+    def get(self, request, *args, **kwargs):
+        self.groundwork(request, args, kwargs)
+        if not self.costume.is_current:
+            return HttpResponseRedirect(
+                reverse('costume_view', urlconf='gbe.urls', args=[self.costume_id]))
+
+        return self.bid_review_response(request)
+
+
+    def post(self, request, *args, **kwargs):
+        self.groundwork(request, args, kwargs)
+        if not self.costume.is_current:
+            return HttpResponseRedirect(
+                reverse('costume_view', urlconf='gbe.urls', args=[self.costume_id]))
+
+        if self.form.is_valid():
             evaluation = form.save(commit=False)
-            evaluation.evaluator = reviewer
-            evaluation.bid = costume
+            evaluation.evaluator = self.reviewer
+            evaluation.bid = self.costume
             evaluation.save()
             return HttpResponseRedirect(reverse('costume_review_list',
                                                 urlconf='gbe.urls'))
         else:
-            return render(request, 'gbe/bid_review.tmpl',
-                          {'readonlyform': [
-                                costume_form,
-                                details,
-                                performer,
-                                profile],
-                           'reviewer': reviewer,
-                           'form': form,
-                           'actionform': actionform,
-                           'actionURL': actionURL,
-                           'conference': conference,
-                           'old_bid': old_bid,
-                           })
-    else:
-        form = BidEvaluationForm(instance=bid_eval)
-        return render(request,
-                      'gbe/bid_review.tmpl',
-                      {'readonlyform': [
-                            costume_form,
-                            details,
-                            performer,
-                            profile],
-                       'reviewer': reviewer,
-                       'form': form,
-                       'actionform': actionform,
-                       'actionURL': actionURL,
-                       'conference': conference,
-                       'old_bid': old_bid,
-                       })
+            return self.bid_review_response(request)
