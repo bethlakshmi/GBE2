9c17d4123d2fca1d505647f9a55eb9ddbceb8df8
Jon Kiparsky <jon@kiparsky.net>
2016-09-04 15:38:26 -0400

    GBE-110: remove debug breakpoints

--- expo/gbe/views/review_bid_view.py
+++ expo/gbe/views/review_bid_view.py
@@ -54,7 +54,6 @@ class ReviewBidView(View):
                                               initial=initial)
 
     def post_response_for_form(self, request):
-        import pdb; pdb.set_trace()
         if self.form.is_valid():
             evaluation = self.form.save(commit=False)
             evaluation.evaluator = self.reviewer
@@ -89,7 +88,6 @@ class ReviewBidView(View):
             self.actionform = False
             self.actionURL = False
         self.conference, self.old_bid = get_conf(self.object)
-        import pdb; pdb.set_trace()
         self.bid_eval = self.bid_evaluation_type.objects.filter(
             bid_id=self.object.pk,
             evaluator_id=self.reviewer.resourceitem_id).first()
