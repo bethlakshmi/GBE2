1f6b3ec39a750afd436b7322b3598bd5fa3aa7dc
bethlakshmi <bethlakshmi@gmail.com>
2016-10-01 23:16:32 -0400

    remove unused code
    Doesn’t seem to be necessary.

--- expo/gbe/forms/show_vote_field.py
+++ expo/gbe/forms/show_vote_field.py
@@ -22,20 +22,7 @@ class ShowVoteField(MultiValueField):
             *args,
             **kwargs)
 
-    def get_prep_value(self, value):
-        if isinstance(value, ShowVote):
-            return value
-        return ShowVote.objects.get(pk=value)
-
     def clean(self, value):
         if isinstance(value, ShowVote):
             return value
         raise Exception("failed in ShowVoteField.clean")
-
-    def compress(self, data_list):
-        if data_list:
-            if any([datum in self.empty_values for datum in data_list]):
-                raise ValidationError()
-            return ShowVote(show_pk=data_list[0],
-                            vote=data_list[1])
-        return None
