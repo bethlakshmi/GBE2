c19b043f8b24132bc75570caddaf2a3d3c4e466f
bethlakshmi <bethlakshmi@gmail.com>
2016-12-13 09:02:47 -0500

    more pep8

--- expo/tests/functions/scheduler_functions.py
+++ expo/tests/functions/scheduler_functions.py
@@ -14,6 +14,7 @@ from gbe_forms_text import (
     class_schedule_options,
 )
 
+
 def book_worker_item_for_role(workeritem, role, eventitem=None):
         worker = WorkerFactory.create(
             _item=workeritem,
@@ -60,14 +61,14 @@ def assert_good_sched_event_form(response, eventitem):
         'type="text" value="01:00:00" />' in response.content
     if eventitem.__class__.__name__ == "Class":
         for label, detail in [
-            (classbid_labels['schedule_constraints'], ', '.join(
-                [j for i,j in class_schedule_options
-                 if i in eventitem.schedule_constraints])),
-            (classbid_labels['avoided_constraints'], ', '.join(
-                [j for i,j in class_schedule_options
-                 if i in eventitem.avoided_constraints])),
-            ('Format', eventitem.type),
-            ('Space Needs', eventitem.get_space_needs_display())]:
+                (classbid_labels['schedule_constraints'], ', '.join(
+                    [j for i, j in class_schedule_options
+                     if i in eventitem.schedule_constraints])),
+                (classbid_labels['avoided_constraints'], ', '.join(
+                    [j for i, j in class_schedule_options
+                     if i in eventitem.avoided_constraints])),
+                ('Format', eventitem.type),
+                ('Space Needs', eventitem.get_space_needs_display())]:
             assert_label(response, label, detail)
 
 
@@ -87,8 +88,9 @@ def assert_link(response, link):
     selection = '<a href="%s">' % link
     assert selection in response.content
 
+
 def assert_label(response, label, details):
-    selection = '<label class="sched_detail">%s:</label></br>%s</br></br>' %(
+    selection = '<label class="sched_detail">%s:</label></br>%s</br></br>' % (
         label,
         details
     )
