f616bb8bc893d91b8a21b65bdb3653ee128acd5a
bethlakshmi <bethlakshmi@gmail.com>
2016-12-31 15:23:23 -0500

    better tests

--- expo/tests/ticketing/test_index.py
+++ expo/tests/ticketing/test_index.py
@@ -20,11 +20,23 @@ class TestTicketingIndex(TestCase):
         self.factory = RequestFactory()
         self.client = Client()
 
-    def test_index(self):
+    def test_one_ticket(self):
         '''
            user gets the list
         '''
-        ticket = TicketItemFactory()
+        ticket = TicketItemFactory(live=True)
+        request = self.factory.get(
+            reverse('index', urlconf='ticketing.urls'),
+        )
+        request.user = UserFactory()
+        request.session = {'cms_admin_site': 1}
+        response = index(request)
+        assert ticket.title in response.content
+
+    def test_no_ticket(self):
+        '''
+           user gets the list
+        '''
         request = self.factory.get(
             reverse('index', urlconf='ticketing.urls'),
         )
@@ -32,3 +44,23 @@ class TestTicketingIndex(TestCase):
         request.session = {'cms_admin_site': 1}
         response = index(request)
         nt.assert_equal(response.status_code, 200)
+
+    def test_one_ticket_per_event(self):
+        '''
+           user gets the list
+        '''
+        not_shown = TicketItemFactory(
+            live=True,
+            cost=1.00,
+            title='Do Not Show Me')
+        ticket = TicketItemFactory(live=True,
+                                   bpt_event=not_shown.bpt_event)
+        request = self.factory.get(
+            reverse('index', urlconf='ticketing.urls'),
+        )
+        request.user = UserFactory()
+        request.session = {'cms_admin_site': 1}
+        response = index(request)
+        assert ticket.title in response.content
+        assert not_shown.title not in response.content
+
