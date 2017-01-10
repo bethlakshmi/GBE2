932db5d5df0ec91bb47e5d2713e57108d44bb854
bethlakshmi <bethlakshmi@gmail.com>
2017-01-09 23:11:40 -0500

    two tests, consistently failing
    May need to pick this up post expo.

--- expo/tests/gbe/test_admin_changelist.py
+++ expo/tests/gbe/test_admin_changelist.py
@@ -1,4 +1,7 @@
-from django.test import TestCase
+from django.test import (
+    Client,
+    TestCase
+)
 from django.test.client import RequestFactory
 from django.contrib.admin.views.main import ALL_VAR, SEARCH_VAR, ChangeList
 from gbe.admin import (
@@ -13,22 +16,35 @@ from gbe.models import (
 )
 from tests.factories.gbe_factories import(
     GenericEventFactory,
+    ProfileFactory,
+    UserFactory,
     VolunteerInterestFactory,
     VolunteerWindowFactory,
 )
 from django.contrib.admin.sites import AdminSite
+from tests.functions.gbe_functions import login_as
 
 
 class GBEChangeListTests(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
+        self.client = Client()
+        self.privileged_user = ProfileFactory()
+        self.privileged_user.user_object.is_superuser = True
+        self.privileged_user.user_object.save()
 
-    def test_select_volunteer_interest_conference(self):
+    def test_get_volunteer_interest_conference(self):
         obj = VolunteerInterestFactory()
         m = VolunteerInterestAdmin(VolunteerInterest, AdminSite)
-        request = self.factory.get('/volunteerinterest/')
-        cl = ChangeList(
-            request, VolunteerInterest,
-            *get_changelist_args(m, list_select_related=m.get_list_select_related(request))
-        )
-        self.assertEqual(cl.queryset.query.select_related, {'parent': {}})
+        login_as(self.privileged_user, self)
+        response = self.client.get('/admin/gbe/volunteerinterest/')
+        print response.content
+        assert str(obj.volunteer.conference) in response.content
+
+    def test_get_volunteer_window_conference(self):
+        obj = VolunteerWindowFactory()
+        m = VolunteerWindowAdmin(VolunteerWindow, AdminSite)
+        login_as(self.privileged_user, self)
+        response = self.client.get('/admin/gbe/volunteerwindow/')
+        print response.content
+        assert str(obj.day.conference) in response.content
