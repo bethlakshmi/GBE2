0fe05785fb859edd55e914974a08ef3c48d07ab2
bethlakshmi <bethlakshmi@gmail.com>
2017-01-10 23:21:48 -0500

    Slicing of scheduler.model
    There was a semi-clean way to separate the model-dependent calls.
    
    Could be better but it pulls apart some cross-app imports.

--- expo/gbe/views/bios_teachers_view.py
+++ expo/gbe/views/bios_teachers_view.py
@@ -1,12 +1,9 @@
 from django.shortcuts import render
-from django.db.models import Q
 from django.core.urlresolvers import reverse
-
 from expo.gbe_logging import log_func
 
-from scheduler.models import (
-    ResourceAllocation,
-    Worker,
+from scheduler.functions import (
+    get_scheduled_events_by_role,
 )
 from gbe.models import Performer
 from gbe.functions import (
@@ -22,27 +19,23 @@ def BiosTeachersView(request):
     Display the teachers bios.  Teachers are anyone teaching,
     moderating or being a panelist.
     '''
-    current_conf = get_current_conference()
     conf_slug = request.GET.get('conference', None)
     if not conf_slug:
-        conference = current_conf
+        conference = get_current_conference()
     else:
         conference = get_conference_by_slug(conf_slug)
     conferences = conference_list()
-
     performers = Performer.objects.all()
-    commits = ResourceAllocation.objects.all()
-    workers = Worker.objects.filter(Q(role="Teacher") |
-                                    Q(role="Moderator") |
-                                    Q(role="Panelist"))
     bios = []
+    workers, commits = get_scheduled_events_by_role(
+        conference,
+        ["Teacher", "Moderator", "Panelist"])
 
     for performer in performers:
         classes = []
         for worker in workers.filter(_item=performer):
             for commit in commits.filter(resource=worker):
-                if commit.event.eventitem.get_conference() == conference:
-                    classes += [{'role': worker.role, 'event': commit.event}]
+                classes += [{'role': worker.role, 'event': commit.event}]
         if len(classes) > 0:
             bios += [{'bio': performer, 'classes': classes}]
 
