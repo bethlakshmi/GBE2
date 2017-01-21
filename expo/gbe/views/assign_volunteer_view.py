32f9613c1cdd2c7a29e5a8d25f18c304b60d0819
bethlakshmi <bethlakshmi@gmail.com>
2017-01-20 22:40:56 -0500

    cache it

--- expo/gbe/views/assign_volunteer_view.py
+++ expo/gbe/views/assign_volunteer_view.py
@@ -17,7 +17,6 @@ from django.http import HttpResponseRedirect
 
 @login_required
 @log_func
-@never_cache
 def AssignVolunteerView(request, volunteer_id):
     '''
     Show a bid  which needs to be assigned to shifts by the coordinator.
