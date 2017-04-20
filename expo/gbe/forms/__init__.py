5a4a11adf409a214c9a7a08420236c50957d7222
bethlakshmi <bethlakshmi@gmail.com>
2017-04-20 07:57:26 -0400

    init

--- expo/gbe/forms/__init__.py
+++ expo/gbe/forms/__init__.py
@@ -16,13 +16,12 @@ from remains import (
     CostumeBidSubmitForm,
     CostumeDetailsDraftForm,
     CostumeDetailsSubmitForm,
-    StageInfoForm,
-    StageInfoSubmitForm,
 )
 from contact_form import ContactForm
 from participant_form import ParticipantForm
 from profile_admin_form import ProfileAdminForm
 from profile_preferences_form import ProfilePreferencesForm
+from user_create_form import UserCreateForm
 
 from persona_form import PersonaForm
 from combo_form import ComboForm
@@ -40,10 +39,17 @@ from bid_evaluation_form import BidEvaluationForm
 from act_bid_evaluation_form import ActBidEvaluationForm
 from bid_state_change_form import BidStateChangeForm
 
+from vendor_bid_form import VendorBidForm
 from volunteer_bid_form import VolunteerBidForm
 from volunteer_interest_form import VolunteerInterestForm
 from volunteer_opportunity_form import VolunteerOpportunityForm
 
+from act_tech_info_form import ActTechInfoForm
 from cue_info_form import CueInfoForm
 from lighting_info_form import LightingInfoForm
+from rehearsal_selection_form import RehearsalSelectionForm
+from stage_info_form import (
+    StageInfoForm,
+    StageInfoSubmitForm
+)
 from vendor_cue_info_form import VendorCueInfoForm
