9f1281a9a5bdf079eeddfa882ab83132bbcc54a2
bethlakshmi <bethlakshmi@gmail.com>
2017-06-30 20:49:47 -0400

    refractor for regular performer grid

--- expo/tests/scheduler/test_detail_view.py
+++ expo/tests/scheduler/test_detail_view.py
@@ -1,5 +1,4 @@
 from django.core.urlresolvers import reverse
-from django.core.exceptions import PermissionDenied
 from tests.factories.gbe_factories import (
     ProfileFactory,
     ShowFactory,
@@ -13,43 +12,60 @@ from django.test import (
     Client,
     TestCase,
 )
+from tests.contexts import ActTechInfoContext
+from gbe.models import Conference
+from scheduler.models import EventItem
+from tests.functions.gbe_functions import bad_id_for
 
 
 class TestDetailView(TestCase):
     view_name = 'detail_view'
 
     def setUp(self):
+        Conference.objects.all().delete()
         self.client = Client()
-        self.show = ShowFactory()
-        self.sched_event = SchedEventFactory.create(
-            eventitem=self.show.eventitem_ptr)
-        self.url = reverse(self.view_name,
-                      urlconf="scheduler.urls",
-                      args=[self.sched_event.pk])
+        self.context = ActTechInfoContext()
+        self.url = reverse(
+            self.view_name,
+            urlconf="scheduler.urls",
+            args=[self.context.show.pk])
 
     def test_no_permission_required(self):
         response = self.client.get(self.url)
         self.assertEqual(200, response.status_code)
-        self.assertContains(response, self.show.e_title)
-
+        self.assertContains(response, self.context.show.e_title)
 
     def test_bad_id_raises_404(self):
         bad_url = reverse(self.view_name,
                       urlconf="scheduler.urls",
-                      args=[self.sched_event.pk+1])
+                      args=[bad_id_for(EventItem)])
         response = self.client.get(bad_url)
         self.assertEqual(response.status_code, 404)
 
     def test_repeated_lead_shows_once(self):
-        sched_event2 = SchedEventFactory.create(
-            eventitem=self.show.eventitem_ptr)
-        sched_events = [self.sched_event, sched_event2]
+        show = ShowFactory()
+        sched_events = [
+            SchedEventFactory.create(
+                eventitem=show.eventitem_ptr) for i in range(2)]
         staff_lead = ProfileFactory()
         lead_worker = WorkerFactory(_item=staff_lead.workeritem_ptr,
                                     role="Staff Lead")
         for se in sched_events:
             ResourceAllocationFactory.create(event=se,
                                              resource=lead_worker)
-        response = self.client.get(self.url)
+        response = self.client.get(reverse(
+            self.view_name,
+            urlconf="scheduler.urls",
+            args=[show.eventitem.pk]))
         self.assertEqual(200, response.status_code)
         self.assertEqual(1, response.content.count(staff_lead.display_name))
+
+    def test_bio_grid(self):
+        self.context.performer.homepage = "www.testhomepage.com"
+        self.context.performer.save()
+        url = reverse(self.view_name,
+                      urlconf="scheduler.urls",
+                      args=[self.context.show.pk])
+        response = self.client.get(url)
+        self.assertEqual(200, response.status_code)
+        self.assertContains(response, self.context.performer.homepage)
