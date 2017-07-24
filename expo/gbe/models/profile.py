7627434fef5a2a82742f716d383b0b3cb2924667
bethlakshmi <bethlakshmi@gmail.com>
2017-07-24 10:53:11 -0400

    Make shows with nested acts

--- expo/gbe/models/profile.py
+++ expo/gbe/models/profile.py
@@ -247,10 +247,14 @@ class Profile(WorkerItem):
     def get_shows(self):
         from gbe.models import Show  # late import, circularity
         acts = self.get_acts()
-        shows = [Show.objects.filter(
-            scheduler_events__resources_allocated__resource__actresource___item=act)
-            for act in acts if act.accepted == 3 and act.is_current]
-        return sum([list(s) for s in shows], [])
+        shows = []
+        for act in acts:
+            if act.accepted == 3 and act.is_current:
+                for show in Show.objects.filter(
+                        scheduler_events__resources_allocated__resource__actresource___item=act):
+                    shows += [(show, act)]
+        shows = sorted(shows, key=lambda show: show[0].e_title)
+        return shows
 
     def get_schedule(self, conference=None):
         '''
