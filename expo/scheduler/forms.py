f6c44a3a069867d59acce2755aab99db3bd29ee3
bethlakshmi <bethlakshmi@gmail.com>
2017-07-30 22:54:12 -0400

    can't remove yet

--- expo/scheduler/forms.py
+++ expo/scheduler/forms.py
@@ -10,10 +10,12 @@ from gbe_forms_text import *
 from gbe.expoformfields import DurationFormField
 from gbe.functions import get_current_conference
 from gbe.forms.common_queries import (
+    visible_personas,
     visible_profiles,
 )
 import pytz
 from gbe.models import Show
+from gbe.scheduling.forms.schedule_selection_form import conference_times
 
 
 class ActScheduleForm(forms.Form):
@@ -62,3 +64,62 @@ class EventItemScheduleForm(forms.ModelForm):
     class Meta:
         model = EventItem
         fields = '__all__'
+
+# DEPRECATED - remove after refactor done
+class EventScheduleForm(forms.ModelForm):
+    required_css_class = 'required'
+    error_css_class = 'error'
+
+    day = forms.ChoiceField(choices=['No Days Specified'])
+    time = forms.ChoiceField(choices=conference_times)
+    location = forms.ModelChoiceField(
+            queryset=LocationItem.objects.all().order_by('room__name'))
+    duration = DurationFormField(
+                   help_text=scheduling_help_texts['duration'])
+    teacher = forms.ModelChoiceField(queryset=visible_personas,
+                                     required=False)
+    moderator = forms.ModelChoiceField(queryset=visible_personas,
+                                       required=False)
+    panelists = forms.ModelMultipleChoiceField(
+        queryset=visible_personas,
+        required=False)
+    staff_lead = forms.ModelChoiceField(queryset=visible_profiles,
+                                        required=False)
+    description = forms.CharField(required=False,
+                                  widget=forms.Textarea,
+                                  help_text=scheduling_help_texts
+                                  ['description'])
+    title = forms.CharField(required=False,
+                            help_text=scheduling_help_texts['title'])
+
+    def __init__(self, *args, **kwargs):
+        conference = get_current_conference()
+        super(EventScheduleForm, self).__init__(*args, **kwargs)
+        self.fields['day'] = forms.ModelChoiceField(
+            queryset=conference.conferenceday_set.all())
+
+    class Meta:
+        model = Event
+        fields = ['day',
+                  'time',
+                  'location',
+                  'duration',
+                  'max_volunteer',
+                  'teacher',
+                  'moderator',
+                  'panelists',
+                  'staff_lead',
+                  'description']
+        help_texts = scheduling_help_texts
+
+    def save(self, commit=True):
+        data = self.cleaned_data
+        event = super(EventScheduleForm, self).save(commit=False)
+        day = data.get('day').day
+        time_parts = map(int, data.get('time').split(":"))
+        starttime = time(*time_parts, tzinfo=pytz.utc)
+        event.starttime = datetime.combine(day, starttime)
+
+        if commit:
+            self.save()
+        return event
