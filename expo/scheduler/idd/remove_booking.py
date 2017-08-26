12b917cc220ac4bc175e301b858ed85614d833df
bethlakshmi <bethlakshmi@gmail.com>
2017-08-26 10:25:03 -0400

    review issue
    - shouldn’t reference a potentially null value (booking_id_)

--- expo/scheduler/idd/remove_booking.py
+++ expo/scheduler/idd/remove_booking.py
@@ -7,17 +7,15 @@ from scheduler.data_transfer import (
 
 
 def remove_booking(occurrence_id,
-                   booking_id=None):
+                   booking_id):
     response = PersonResponse()
-    if booking_id:
-        test = test_booking(booking_id, occurrence_id)
-        if not test:
-            response.errors = [Error(
-                code="BOOKING_NOT_FOUND",
-                details="Could not find booking id " +
-                "%d for occurrence id %d." % (
-                    booking_id, occurrence_id))]
-            return response
+    test = test_booking(booking_id, occurrence_id)
+    if not test:
+        response.errors = [Error(
+            code="BOOKING_NOT_FOUND",
+            details="Could not find booking id %d for occurrence id %d." % (
+                booking_id, occurrence_id))]
+        return response
     ResourceAllocation.objects.filter(pk=booking_id).delete()
     response.booking_id = booking_id
     return response
