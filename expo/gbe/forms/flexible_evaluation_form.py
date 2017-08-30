711084493f8a7dad91b931988022f945d130b23d
bethlakshmi <bethlakshmi@gmail.com>
2017-08-30 08:16:35 -0400

    looks good, does nothing

--- expo/gbe/forms/flexible_evaluation_form.py
+++ expo/gbe/forms/flexible_evaluation_form.py
@@ -2,8 +2,17 @@ from django.forms import (
     ChoiceField,
     HiddenInput,
     ModelForm,
+    RadioSelect,
 )
 from gbe.models import FlexibleEvaluation
+
+
+class HorizontalRadioSelect(RadioSelect):
+
+    def __init__(self, *args, **kwargs):
+        super(HorizontalRadioSelect, self).__init__(*args, **kwargs)
+
+        self.renderer.inner_html = '<td>{choice_value}{sub_widgets}</td>'
 
 
 class FlexibleEvaluationForm(ModelForm):
@@ -12,12 +21,16 @@ class FlexibleEvaluationForm(ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(FlexibleEvaluationForm, self).__init__(*args, **kwargs)
+        choice_set = [("", "")]
+        choice_set += [(i, "") for i in range(0, 6)]
         if 'initial' in kwargs:
             initial = kwargs.pop('initial')
             self.fields['ranking'] = ChoiceField(
                 label=initial['category'].category,
                 help_text=initial['category'].help_text,
-                required=False)
+                required=False,
+                widget=HorizontalRadioSelect(),
+                choices=choice_set)
 
     class Meta:
         model = FlexibleEvaluation
