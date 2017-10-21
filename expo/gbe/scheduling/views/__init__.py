d118bd8057ae94cb364d39be1d9261f3079aac41
bethlakshmi <bethlakshmi@gmail.com>
2017-10-21 12:55:15 -0400

    Revert "First pick"
    This reverts commit bca4ba36d652ebaa314052dec7cffe5bb8dd91f3.

--- expo/gbe/scheduling/views/__init__.py
+++ expo/gbe/scheduling/views/__init__.py
@@ -3,3 +3,6 @@ from create_event_view import CreateEventView
 from manage_vol_ops_view import ManageVolOpsView
 from show_calendar_view import ShowCalendarView
 from allocate_worker_view import AllocateWorkerView
+from create_event_view import CreateEventView
+from event_wizard_view import EventWizardView
+from class_wizard_view import ClassWizardView
