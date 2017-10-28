a30e78b1e572a40f312c25992000370510187cd2
bethlakshmi <bethlakshmi@gmail.com>
2017-10-27 23:13:24 -0400

    more

--- expo/tests/gbe/scheduling/test_class_wizard.py
+++ expo/tests/gbe/scheduling/test_class_wizard.py
@@ -5,6 +5,7 @@ from django.core.urlresolvers import reverse
 from tests.factories.gbe_factories import (
     ClassFactory,
     ConferenceFactory,
+    ConferenceDayFactory,
     ProfileFactory,
 )
 from tests.functions.gbe_functions import (
@@ -13,6 +14,7 @@ from tests.functions.gbe_functions import (
 )
 from gbe_forms_text import event_type_options
 from tests.functions.gbe_scheduling_functions import assert_event_was_picked_in_wizard
+from expo.settings import DATE_FORMAT
 
 
 class TestClassWizard(TestCase):
@@ -21,6 +23,7 @@ class TestClassWizard(TestCase):
 
     def setUp(self):
         self.current_conference = ConferenceFactory(accepting_bids=True)
+        self.day = ConferenceDayFactory(conference=self.current_conference)
         self.test_class = ClassFactory(b_conference=self.current_conference,
                                        e_conference=self.current_conference,
                                        accepted=3,
@@ -54,6 +57,14 @@ class TestClassWizard(TestCase):
         self.assertContains(response, str(self.test_class.b_title))
         self.assertContains(response, str(self.test_class.teacher))
 
+    def test_authorized_user_single_conference(self):
+        other_class = ClassFactory(accepted=3,
+                                   submitted=True)
+        login_as(self.privileged_user, self)
+        response = self.client.get(self.url)
+        self.assertNotContains(response, str(other_class.b_title))
+        self.assertNotContains(response, str(other_class.teacher))
+
     def test_auth_user_can_pick_class(self):
         login_as(self.privileged_user, self)
         data = self.get_data()
@@ -77,3 +88,29 @@ class TestClassWizard(TestCase):
         self.assertContains(
             response,
             'That choice is not one of the available choices.')
+
+    def test_auth_user_load_class(self):
+        login_as(self.privileged_user, self)
+        data = self.get_data()
+        response = self.client.post(
+            self.url,
+            data=data,
+            follow=True)
+        self.assertContains(
+            response,
+            'value="%s"' %
+            self.test_class.b_title)
+        self.assertContains(
+            response,
+            'type="number" value="1.0"')
+        self.assertContains(
+            response,
+            '<option value="%d">%s</option>' % (
+                self.day.pk,
+                self.day.day.strftime(DATE_FORMAT)
+            ))
+        self.assertContains(
+            response,
+            '<option value="%d" selected="selected">%s</option>' % (
+                self.test_class.teacher.pk,
+                str(self.test_class.teacher)))
