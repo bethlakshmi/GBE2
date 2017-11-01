7f100bcf0d484f0ae9505e470ed71adf3c0dc7d6
bethlakshmi <bethlakshmi@gmail.com>
2017-11-01 08:33:11 -0400

    validation and better error display

--- expo/gbe/scheduling/forms/copy_event_pick_target_form.py
+++ expo/gbe/scheduling/forms/copy_event_pick_target_form.py
@@ -70,3 +70,16 @@ class CopyEventPickModeForm(CopyEventPickDayForm):
         else:
             self.fields['target_event'].queryset = Event.objects.exclude(
                 e_conference__status="completed")
+
+    def clean(self):
+        cleaned_data = super(CopyEventPickModeForm, self).clean()
+        copy_mode = cleaned_data.get("copy_mode")
+        target_event = cleaned_data.get("target_event")
+        copy_to_day = cleaned_data.get("copy_to_day")
+        if copy_mode:
+            if copy_mode == copy_mode_choices[0][0] and not target_event:
+                msg = " Must choose the target event when copying sub-events."
+                self.add_error('target_event', msg)
+            if copy_mode == copy_mode_choices[1][0] and not copy_to_day:
+                msg = " Must choose a day when copying all events."
+                self.add_error('copy_to_day', msg)
