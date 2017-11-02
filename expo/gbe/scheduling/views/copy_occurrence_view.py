1e74d0aecce0e12dc571a486266f4a5c17ad1196
bethlakshmi <bethlakshmi@gmail.com>
2017-11-02 08:54:28 -0400

    make it's own wizard

--- expo/gbe/scheduling/views/copy_occurrence_view.py
+++ expo/gbe/scheduling/views/copy_occurrence_view.py
@@ -45,7 +45,7 @@ from gbe_forms_text import (
 
 
 class CopyOccurrenceView(View):
-    template = 'gbe/scheduling/event_wizard.tmpl'
+    template = 'gbe/scheduling/copy_wizard.tmpl'
     permissions = ('Scheduling Mavens',)
 
     occurrence = None
@@ -88,6 +88,47 @@ class CopyOccurrenceView(View):
             context['pick_day'].fields['copy_to_day'].required = True
         return context
 
+    def validate_and_proceed(self, context):
+        initial = {}
+        if 'copy_mode' in context.keys() and context['copy_mode'].is_valid():
+            context['second_form'] = True
+            if context['copy_mode'].cleaned_data[
+                    'copy_mode'] == "copy_children_only":
+                context['second_title'] = "Destination is %s: %s" % (
+                    context['copy_mode'].cleaned_data[
+                        'target_event'].e_conference.conference_slug,
+                    context['copy_mode'].cleaned_data['target_event'].e_title)
+                initial = {
+                    'conference': context['copy_mode'].cleaned_data[
+                        'target_event'].e_conference,
+                    'new_parent': context['copy_mode'].cleaned_data['target_event'],
+                }
+            elif context['copy_mode'].cleaned_data[
+                    'copy_mode'] == "include_parent":
+                context['second_title'] = "Create Copied Event at %s: %s" % (
+                    context['copy_mode'].cleaned_data[
+                        'copy_to_day'].conference.conference_slug,
+                    str(context['copy_mode'].cleaned_data['copy_to_day']))
+                initial = {
+                    'conference': context['copy_mode'].cleaned_data[
+                        'copy_to_day'].conference,
+                    'day': context['copy_mode'].cleaned_data['copy_to_day'],
+                    'include_parent': True,
+                }
+        elif 'pick_day' in context.keys() and context['pick_day'].is_valid():
+            context['second_form'] = True
+            context['second_title'] = "Create Copied Event at %s: %s" % (
+                context['pick_day'].cleaned_data[
+                    'copy_to_day'].conference.conference_slug,
+                    str(context['pick_day'].cleaned_data['copy_to_day']))
+            initial = {
+                'conference': context['pick_day'].cleaned_data[
+                        'copy_to_day'].conference,
+                'day': context['pick_day'].cleaned_data['copy_to_day'],
+                'include_parent': True,
+            }
+        return initial, context
+
     @never_cache
     def get(self, request, *args, **kwargs):
         self.groundwork(request, args, kwargs)
@@ -96,11 +137,9 @@ class CopyOccurrenceView(View):
     @never_cache
     def post(self, request, *args, **kwargs):
         self.groundwork(request, args, kwargs)
-        context = self.build_mode_form(request)
-        if context['copy_mode'].is_valid():
-            context['second_form'] = True
-            context['second_title'] = "Copying %s to ..." % (
-                self.occurrence.eventitem.event.e_title)
+        if 'pick_event' in request.POST.keys():
+            context = self.build_mode_form(request)
+            initial, context = self.validate_and_proceed(context)
         return render(
             request,
             self.template,
