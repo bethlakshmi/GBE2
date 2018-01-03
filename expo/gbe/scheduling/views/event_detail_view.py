4b84a74249431df9ecb379e21357cf25ae505e98
bethlakshmi <bethlakshmi@gmail.com>
2018-01-03 13:09:59 -0500

    Refactor stars to fix design bug

--- expo/gbe/scheduling/views/event_detail_view.py
+++ expo/gbe/scheduling/views/event_detail_view.py
@@ -6,6 +6,7 @@ from gbe.scheduling.views.functions import (
     get_event_display_info,
 )
 from scheduler.idd import get_schedule
+from django.core.urlresolvers import reverse
 
 
 class EventDetailView(View):
@@ -15,22 +16,36 @@ class EventDetailView(View):
     '''
     def get(self, request, *args, **kwargs):
         eventitem_id = kwargs['eventitem_id']
-        toggle = "on"
+        schedule_items = []
         eventitem_view = get_event_display_info(eventitem_id)
-        if eventitem_view['event'].calendar_type == "Volunteer" or (
+        sched_response = get_schedule(
+            request.user,
+            labels=[eventitem_view['event'].calendar_type,
+                    eventitem_view['event'].e_conference.conference_slug])
+        for occurrence in eventitem_view['scheduled_events']:
+            schedule_item = {
+                'occurrence': occurrence,
+                'favorite_link': reverse(
+                    'set_favorite',
+                    args=[occurrence.pk, 'on'],
+                    urlconf='gbe.scheduling.urls'),
+                'highlight': None
+            }
+            if eventitem_view['event'].calendar_type == "Volunteer" or (
                 eventitem_view['event'].e_conference.status == "completed"):
-            toggle = None
-        elif request.user.is_authenticated() and request.user.profile:
-            sched_response = get_schedule(
-                request.user,
-                labels=[eventitem_view['event'].calendar_type,
-                        eventitem_view['event'].e_conference.conference_slug])
-            for booking in sched_response.schedule_items:
-                if booking.event in eventitem_view['scheduled_events']:
-                    if booking.role == "Interested":
-                        toggle = "off"
-                    else:
-                        toggle = "disabled"
+                schedule_item['favorite_link'] = None
+            elif request.user.is_authenticated() and request.user.profile:
+                for booking in sched_response.schedule_items:
+                    if booking.event == occurrence:
+                        schedule_item['highlight'] = booking.role.lower()
+                        if booking.role == "Interested":
+                            schedule_item['favorite_link'] = reverse(
+                                'set_favorite',
+                                args=[occurrence.pk, 'off'],
+                                urlconf='gbe.scheduling.urls')
+                        else:
+                            schedule_item['favorite_link'] = "disabled"
+            schedule_items += [schedule_item]
         template = 'gbe/scheduling/event_detail.tmpl'
         return render(request,
                       template,
@@ -38,7 +53,7 @@ class EventDetailView(View):
                        'show_tickets': True,
                        'tickets': eventitem_view['event'].get_tickets,
                        'user_id': request.user.id,
-                       'toggle': toggle})
+                       'schedule_items': schedule_items})
 
     def dispatch(self, *args, **kwargs):
         return super(EventDetailView, self).dispatch(*args, **kwargs)
