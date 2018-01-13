cfad86477046b0fa658b0f31a86eadf4757e94ff
bethlakshmi <bethlakshmi@gmail.com>
2018-01-13 16:03:48 -0500

    limit to volunteer coordinator

--- expo/gbe/scheduling/views/ticketed_event_wizard_view.py
+++ expo/gbe/scheduling/views/ticketed_event_wizard_view.py
@@ -43,7 +43,8 @@ class TicketedEventWizardView(EventWizardView):
             self.event_type]['second_title']
         context['tickets'] = None
         context['volunteer_scheduling'] = event_settings[
-            self.event_type]['volunteer_scheduling']
+            self.event_type]['volunteer_scheduling'] and validate_perms(request,
+                          ('Volunteer Coordinator',), require=False)
         return context
 
     def setup_ticket_links(self, request, new_event, ticket_form):
