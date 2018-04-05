0b90bb7f173b2ed256d355f85cd7a500d9300f8f
bethlakshmi <bethlakshmi@gmail.com>
2018-04-04 23:11:25 -0400

    Revert "allocate works"
    This reverts commit daab3464e25b83d8bfcda080864ac8b01a062eba.

--- expo/gbe/scheduling/views/edit_volunteer_view.py
+++ expo/gbe/scheduling/views/edit_volunteer_view.py
@@ -3,9 +3,21 @@ from django.views.decorators.cache import never_cache
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import (
+    get_object_or_404,
+    render,
+)
 from django.core.urlresolvers import reverse
+from gbe.models import (
+    Conference,
+    Event,
+)
 from gbe.functions import validate_perms
+from gbe_forms_text import (
+    role_map,
+    event_settings,
+)
+from scheduler.idd import get_occurrence
 from gbe.scheduling.views.functions import (
     process_post_response,
     setup_event_management_form,
@@ -34,17 +46,23 @@ class EditVolunteerView(ManageWorkerView):
             return HttpResponseRedirect("%s?%s" % (
                 reverse('edit_event',
                         urlconf='gbe.scheduling.urls',
-                        args=[self.conference.conference_slug,
+                        args=[self.item.e_conference.conference_slug,
                               self.occurrence.pk]),
                 request.GET.urlencode()))
         self.manage_worker_url = reverse('manage_workers',
                                          urlconf='gbe.scheduling.urls',
-                                         args=[self.conference.conference_slug,
+                                         args=[self.item.e_conference.conference_slug,
                                                self.occurrence.pk])
         self.success_url = reverse('edit_volunteer',
                                    urlconf='gbe.scheduling.urls',
-                                   args=[self.conference.conference_slug,
+                                   args=[self.item.e_conference.conference_slug,
                                          self.occurrence.pk])
+
+    def is_formset_valid(self, formset):
+        validity = False
+        for form in formset:
+            validity = form.is_valid() or validity
+        return validity
 
     def make_context(self, request, errorcontext=None):
         context = super(EditVolunteerView,
@@ -78,11 +96,11 @@ class EditVolunteerView(ManageWorkerView):
     @never_cache
     @method_decorator(login_required)
     def post(self, request, *args, **kwargs):
+        if "manage-workers" in request.path:
+            return super(EditVolunteerView, self).post(request, *args, **kwargs)
         error_url = self.groundwork(request, args, kwargs)
         if error_url:
             return error_url
-        if "manage-workers" in request.path:
-            return super(EditVolunteerView, self).post(request, *args, **kwargs)
         context, self.success_url, response = process_post_response(
             request,
             self.conference.conference_slug,
--- expo/gbe/scheduling/views/manage_worker_view.py
+++ expo/gbe/scheduling/views/manage_worker_view.py
@@ -11,27 +11,25 @@ from django.views.generic import View
 from django.forms import HiddenInput
 from django.contrib import messages
 from scheduler.idd import (
+    create_occurrence,
     get_conflicts,
-    remove_booking,
-    set_person,
-    test_booking,
+    get_occurrence,
+    get_occurrences,
+    update_occurrence,
 )
 from gbe.models import (
     Profile,
+    GenericEvent,
+    Room,
     UserMessage,
 )
-from gbe.email.functions import send_schedule_update_mail
-from gbetext import volunteer_allocate_email_fail_msg
 from gbe.scheduling.forms import WorkerAllocationForm
-from gbe.scheduling.views.functions import show_scheduling_booking_status
+from gbe.scheduling.views.functions import (
+    get_start_time,
+    show_scheduling_occurrence_status,
+)
 from gbe.functions import eligible_volunteers
 from gbe_forms_text import rank_interest_options
-from scheduler.data_transfer import (
-    Error,
-    PersonResponse,
-    Person,
-)
-
 
 class ManageWorkerView(View):
     ''' This must be a parent to another class.  The subclass should describe
@@ -45,6 +43,8 @@ class ManageWorkerView(View):
                 - the conference slug, and appropriate calendar type will be
                 made here.
             - self.manage_worker_url - the URL used to call this post function
+            - self.parent_id (optional) - the id of a parent event, if null,
+                there will be no parent
             - self.success_url - the URL to redirect to in the event of success
             - 'make_context' to build the error context when an error occurrs
         OPTIONAL
@@ -59,6 +59,7 @@ class ManageWorkerView(View):
     '''
 
     vol_permissions = ('Volunteer Coordinator',)
+    parent_id = None
     labels = []
     window_controls = ['start_open', 'worker_open']
 
@@ -162,88 +163,156 @@ class ManageWorkerView(View):
                            response=None,
                            errorcontext=None):
         if response:
-            show_scheduling_booking_status(
+            show_scheduling_occurrence_status(
                 request,
                 response,
                 self.__class__.__name__)
-            if response.booking_id:
-                self.success_url = "%s?changed_id=%d" % (
-                    self.success_url,
-                    response.booking_id)
-                return HttpResponseRedirect(self.success_url)
-        return self.make_context(request, errorcontext)
+
+        if response and response.occurrence:
+            return HttpResponseRedirect(self.success_url)
+        else:
+            return render(request,
+                          self.template,
+                          self.make_context(request, errorcontext))
+
+    def check_success_and_return(self,
+                                 request,
+                                 response=None,
+                                 errorcontext=None,
+                                 window_controls="worker_open=True"):
+        if response and response.occurrence:
+            self.success_url = "%s?changed_id=%d" % (
+                self.success_url,
+                response.occurrence.pk)
+            if window_controls:
+                self.success_url = "%s&%s" % (self.success_url,
+                                              window_controls)
+
+        return self.make_post_response(
+            request,
+            response,
+            errorcontext)
+
+    def get_basic_form_settings(self):
+        self.event = self.event_form.save(commit=False)
+        data = self.event_form.cleaned_data
+        self.room = get_object_or_404(Room, name=data['location'])
+        self.max_volunteer = 0
+        if data['max_volunteer']:
+                self.max_volunteer = data['max_volunteer']
+        self.start_time = get_start_time(data)
+        if self.create:
+            data['labels'] = self.labels + [self.conference.conference_slug]
+            if self.event.calendar_type:
+                data['labels'] += [self.event.calendar_type]
+        return data
+
+    def do_additional_actions(self, request):
+        return None
 
     @never_cache
     @method_decorator(login_required)
     def post(self, request, *args, **kwargs):
-        form = WorkerAllocationForm(request.POST)
+        error_url = self.groundwork(request, args, kwargs)
+        if error_url:
+            return error_url
+        self.create = False
         response = None
-        email_status = None
-        if not form.is_valid():
-            context = None
-            if request.POST['alloc_id'] == '-1':
-                form.data['alloc_id'] = -1
-                context = {'new_worker_alloc_form': form}
+        context = None
+
+        if ('create' in request.POST.keys()) or (
+                'duplicate' in request.POST.keys()):
+            self.create = True
+            if 'create' in request.POST.keys():
+                self.event_form = VolunteerOpportunityForm(
+                    request.POST,
+                    prefix='new_opp',
+                    conference=self.conference)
             else:
-                is_present = test_booking(
-                    int(request.POST['alloc_id']), self.occurrence.pk)
-                if not is_present:
-                    response = PersonResponse(errors=[Error(
-                        code="BOOKING_NOT_FOUND",
-                        details="Booking id %s for occurrence %d not found" % (
-                            request.POST['alloc_id'], self.occurrence.pk)), ])
-                context = {'worker_alloc_forms': form}
-            return self.make_post_response(request,
-                                           response=response,
-                                           errorcontext=context)
+                self.event_form = VolunteerOpportunityForm(
+                    request.POST,
+                    conference=self.conference)
+            if self.event_form.is_valid():
+                data = self.get_basic_form_settings()
+                self.event.e_conference = self.conference
+                self.event.save()
+                response = create_occurrence(
+                    self.event.eventitem_id,
+                    self.start_time,
+                    self.max_volunteer,
+                    locations=[self.room],
+                    labels=data['labels'],
+                    parent_event_id=self.parent_id)
+            else:
+                context = {'createform': self.event_form,
+                           'worker_open': True}
+
+            return self.check_success_and_return(request,
+                                                 response=response,
+                                                 errorcontext=context)
+
+        elif 'edit' in request.POST.keys():
+            self.event = get_object_or_404(
+                GenericEvent,
+                event_id=request.POST['opp_event_id'])
+            self.event_form = VolunteerOpportunityForm(
+                request.POST,
+                instance=self.event)
+            if self.event_form.is_valid():
+                data = self.get_basic_form_settings()
+                self.event_form.save()
+                response = update_occurrence(
+                    data['opp_sched_id'],
+                    self.start_time,
+                    self.max_volunteer,
+                    locations=[self.room])
+            else:
+                context = {'error_opp_form': self.event_form,
+                           'worker_open': True}
+
+            return self.check_success_and_return(request,
+                                                 response=response,
+                                                 errorcontext=context)
+
+        elif 'delete' in request.POST.keys():
+            opp = get_object_or_404(
+                GenericEvent,
+                event_id=request.POST['opp_event_id'])
+            title = opp.e_title
+            opp.delete()
+            user_message = UserMessage.objects.get_or_create(
+                view=self.__class__.__name__,
+                code="DELETE_SUCCESS",
+                defaults={
+                    'summary': "Volunteer Opportunity Deleted",
+                    'description': "This volunteer opportunity was deleted."})
+            messages.success(
+                request,
+                '%s<br>Title: %s' % (
+                    user_message[0].description,
+                    title))
+            return HttpResponseRedirect(self.success_url)
+
+        elif 'allocate' in request.POST.keys():
+            response = get_occurrence(request.POST['opp_sched_id'])
+            return HttpResponseRedirect(
+                reverse('edit_event_schedule',
+                        urlconf='gbe.scheduling.urls',
+                        args=['GenericEvent',
+                              response.occurrence.eventitem.eventitem_id,
+                              request.POST['opp_sched_id']]))
         else:
-            data = form.cleaned_data
-            if 'delete' in request.POST.keys():
-                if ('alloc_id' not in request.POST) or (len(
-                        request.POST['alloc_id']) == 0):
-                    return self.make_post_response(
-                        request,
-                        PersonResponse(errors=[Error(
-                            code="NO_BOOKING",
-                            details="No booking id for occurrence id %d." % (
-                                self.occurrence.pk))]))
-                response = remove_booking(
-                    self.occurrence.pk,
-                    booking_id=int(request.POST['alloc_id']))
-                if response.booking_id:
-                    email_status = send_schedule_update_mail(
-                        "Volunteer", data['worker'].workeritem.as_subtype)
-            elif data.get('worker', None):
-                if data['role'] == "Volunteer":
-                    data['worker'].workeritem.as_subtype.check_vol_bid(
-                        self.item.e_conference)
-                person = Person(
-                    user=data['worker'].workeritem.as_subtype.user_object,
-                    public_id=data['worker'].workeritem.as_subtype.pk,
-                    role=data['role'],
-                    label=data['label'],
-                    worker=None)
-                if int(data['alloc_id']) > -1:
-                    person.booking_id = int(data['alloc_id'])
-                response = set_person(
-                    self.occurrence.pk,
-                    person
-                )
-                email_status = send_schedule_update_mail("Volunteer",
-                                                         data['worker'])
-            if email_status:
+            actions = self.do_additional_actions(request)
+            if actions:
+                return actions
+            else:
                 user_message = UserMessage.objects.get_or_create(
                     view=self.__class__.__name__,
-                    code="EMAIL_FAILURE",
+                    code="UNKNOWN_ACTION",
                     defaults={
-                        'summary': "Email Failed",
-                        'description': volunteer_allocate_email_fail_msg})
+                        'summary': "Unknown Action",
+                        'description': "This is an unknown action."})
                 messages.error(
                     request,
                     user_message[0].description)
-            self.success_url = reverse('edit_event',
-                                       urlconf='gbe.scheduling.urls',
-                                       args=[self.conference.conference_slug,
-                                             self.occurrence.pk])
-        return self.make_post_response(request,
-                                       response=response)
+                return HttpResponseRedirect(self.success_url)
