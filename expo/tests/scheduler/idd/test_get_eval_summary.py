d3c5e8a508ac986ced9dc274ef9ee58471588ab4
bethlakshmi <bethlakshmi@gmail.com>
2018-04-08 14:51:49 -0400

    new test, kill excessive caution

--- expo/tests/scheduler/idd/test_get_eval_summary.py
+++ expo/tests/scheduler/idd/test_get_eval_summary.py
@@ -1,16 +1,16 @@
 from django.test import TestCase
-from scheduler.idd import get_eval_info
+from scheduler.idd import get_eval_summary
 from tests.factories.scheduler_factories import EventEvalQuestionFactory
 from scheduler.models import EventEvalQuestion
 
 
-class TestGetEvalInfo(TestCase):
+class TestGetEvalSummary(TestCase):
     view_name = 'export_calendar'
 
     def setUp(self):
         EventEvalQuestion.objects.all().delete()
-        self.question = EventEvalQuestionFactory(visible=False)
 
     def test_get_invisible_question(self):
-        response = get_eval_info(visible=False)
+        self.question = EventEvalQuestionFactory(visible=False)
+        response = get_eval_summary([], visible=False)
         self.assertEqual(response.questions.first(), self.question)
