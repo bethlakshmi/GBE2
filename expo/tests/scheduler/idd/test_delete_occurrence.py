b06a1355cbc3bc9e5b493547df679dab8ae121e0
bethlakshmi <bethlakshmi@gmail.com>
2018-04-07 23:53:05 -0400

    2 tests, 2 bugs

--- expo/tests/scheduler/idd/test_delete_occurrence.py
+++ expo/tests/scheduler/idd/test_delete_occurrence.py
@@ -4,10 +4,6 @@ from django.test import (
 )
 from scheduler.idd import delete_occurrence
 from tests.contexts import VolunteerContext
-from scheduler.models import ( 
-    Event, 
-    EventContainer, 
-)
 
 
 class TestDeleteOccurrence(TestCase):
@@ -20,3 +16,8 @@ class TestDeleteOccurrence(TestCase):
     def test_delete_w_parent(self):
         response = delete_occurrence(self.context.sched_event.pk)
         self.assertEqual(response.warnings[0].code, "PARENT_EVENT_DELETION")
+
+    def test_delete_bad_occurrence(self):
+        event = Event.objects.filter(pk=(self.context.opp_event.pk+1000))
+        response = delete_occurrence(self.context.opp_event.pk+1000)
+        self.assertEqual(response.errors[0].code, "OCCURRENCE_NOT_FOUND")
